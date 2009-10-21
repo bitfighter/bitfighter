effc673d7a8ac271ad3a925e37cd843bbd557c84
eykamp <chris@eykamp.com>
2009-10-21 10:54:44 +0000

    Version bump

--- zap/game.h
+++ zap/game.h
@@ -301,8 +301,8 @@ extern void endGame();
 // already bumped master, release.  cs also ok
 #define MASTER_PROTOCOL_VERSION 2  // Change this when releasing an incompatible cm protocol (must be int)
 #define CS_PROTOCOL_VERSION 18     // Change this when releasing an incompatible cs protocol (must be int)
-#define BUILD_VERSION 485          // Version of the game according to SVN, will be unique every release (must be int)
-#define ZAP_GAME_RELEASE "011 preveiew alpha 4" //"Bitfighter Release Candidate 010"   // Change this with every release -- for display purposes only, string
+#define BUILD_VERSION 509          // Version of the game according to SVN, will be unique every release (must be int)
+#define ZAP_GAME_RELEASE "011 preveiew 5" //"Bitfighter Release Candidate 010"   // Change this with every release -- for display purposes only, string
 
 };
 
