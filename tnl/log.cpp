6e842d5be7f2aea548a5b0bd93e6fdacbe878074
eykamp <chris@eykamp.com>
2010-02-12 07:23:13 +0000

    Added stdio header for newer gcc and/or 64-bit builds

--- tnl/log.cpp
+++ tnl/log.cpp
@@ -29,6 +29,7 @@
 #include <stdarg.h>
 #include <time.h>
 #include <string.h>
+#include <stdio.h>  // For newer versions of gcc?
 
 #ifdef _MSC_VER
 #pragma warning (disable: 4996)     // Disable POSIX deprecation, certain security warnings that seem to be specific to VC++
