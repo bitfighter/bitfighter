30f6d28d94452a085dfeef73786eb274be97cd90
eykamp <chris@eykamp.com>
2010-09-22 15:33:43 +0000

    master mods

--- master/authenticator.cpp
+++ master/authenticator.cpp
@@ -25,7 +25,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "phpbbhash.h"
 #include <string.h>
 #include <ctype.h>
-#include <../mysql++/mysql++.h>
+#include "mysql++/lib/mysql++.h"
 
 using namespace mysqlpp;
 using namespace std;
