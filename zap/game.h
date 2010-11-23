9855023ab20c831093e334e726c9d121a6f160d1
eykamp <chris@eykamp.com>
2010-11-23 16:43:48 +0000

    version number

--- zap/game.h
+++ zap/game.h
@@ -402,7 +402,7 @@ extern void endGame();
 
 #define MASTER_PROTOCOL_VERSION 3  // Change this when releasing an incompatible cm/sm protocol (must be int)
 #define CS_PROTOCOL_VERSION 30     // Change this when releasing an incompatible cs protocol (must be int)
-#define BUILD_VERSION 951          // Version of the game according to SVN, will be unique every release (must be int)
+#define BUILD_VERSION 1082         // Version of the game according to SVN, will be unique every release (must be int)
 #define ZAP_GAME_RELEASE "014 beta 2"     // Change this with every release -- for display purposes only, string, 
                                    // will also be used for name of installer on windows, so be careful with spaces
 };
