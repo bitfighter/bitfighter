a942143615e7d50434b859f1c28aa7f384278bac
eykamp <chris@eykamp.com>
2011-01-25 01:43:04 -0800

    Rearrange so it will compile

--- tnl/tnlTypes.h
+++ tnl/tnlTypes.h
@@ -143,46 +143,6 @@ template<U32 rangeStart, U32 rangeEnd> struct RangedU32 : NetType
    operator U32() const { return value; }
 };
 
-//------------------------------------------------------------------------------
-//-------------------------------------- Type constants...
-
-/// @defgroup BasicConstants Global Constants
-///
-/// Handy constants!
-/// @{
-
-#define __EQUAL_CONST_F F32(0.000001)                            ///< Constant float epsilon used for F32 comparisons
-
-static const F32 FloatOne  = F32(1.0);                           ///< Constant float 1.0
-static const F32 FloatHalf = F32(0.5);                           ///< Constant float 0.5
-static const F32 FloatZero = F32(0.0);                           ///< Constant float 0.0
-
-static const F32 FloatPi   = F32(3.14159265358979323846);            ///< Constant float PI
-static const F32 Float2Pi  = F32(2.0 * 3.14159265358979323846);      ///< Constant float 2*PI
-static const F32 FloatInversePi = F32(1.0 / 3.14159265358979323846); ///< Constant float 1 / PI
-static const F32 FloatHalfPi = F32(0.5 * 3.14159265358979323846);    ///< Constant float 1/2 * PI
-static const F32 Float2InversePi = F32(2.0 / 3.14159265358979323846);///< Constant float 2 / PI
-static const F32 FloatInverse2Pi = F32(0.5 / 3.14159265358979323846);///< Constant float 2 / PI
-
-static const F32 FloatSqrt2 = F32(1.41421356237309504880f);          ///< Constant float sqrt(2)
-static const F32 FloatSqrtHalf = F32(0.7071067811865475244008443f);  ///< Constant float sqrt(0.5)
-
-static const S8  S8_MIN  = S8(-128);                              ///< Constant Min Limit S8
-static const S8  S8_MAX  = S8(127);                               ///< Constant Max Limit S8
-static const U8  U8_MAX  = U8(0xFF);                              ///< Constant Max Limit U8
-
-static const S16 S16_MIN = S16(-32768);                           ///< Constant Min Limit S16
-static const S16 S16_MAX = S16(32767);                            ///< Constant Max Limit S16
-static const U16 U16_MAX = U16(0xFFFF);                           ///< Constant Max Limit U16
-
-static const S32 S32_MIN = S32(-2147483647 - 1);                  ///< Constant Min Limit S32
-static const S32 S32_MAX = S32(2147483647);                       ///< Constant Max Limit S32
-static const U32 U32_MAX = U32(0xFFFFFFFF);                       ///< Constant Max Limit U32
-
-static const F32 F32_MIN = F32(1.175494351e-38F);                 ///< Constant Min Limit F32
-static const F32 F32_MAX = F32(3.402823466e+38F);                 ///< Constant Max Limit F32
-
-static const U64 U64_MAX = U64(0xFFFFFFFFFFFFFFFF);               ///< Constant Max Limit U64
 
 //----------------------------------------------------------------------------------
 // Identify the compiler and OS specific stuff we need:
@@ -238,6 +198,52 @@ typedef unsigned long long  U64;  ///< Compiler independent unsigned 64-bit inte
 #else
 #  error "TNL: Unknown Compiler"
 #endif
+
+
+
+//------------------------------------------------------------------------------
+//-------------------------------------- Type constants...
+
+/// @defgroup BasicConstants Global Constants
+///
+/// Handy constants!
+/// @{
+
+#define __EQUAL_CONST_F F32(0.000001)                            ///< Constant float epsilon used for F32 comparisons
+
+static const F32 FloatOne  = F32(1.0);                           ///< Constant float 1.0
+static const F32 FloatHalf = F32(0.5);                           ///< Constant float 0.5
+static const F32 FloatZero = F32(0.0);                           ///< Constant float 0.0
+
+static const F32 FloatPi   = F32(3.14159265358979323846);            ///< Constant float PI
+static const F32 Float2Pi  = F32(2.0 * 3.14159265358979323846);      ///< Constant float 2*PI
+static const F32 FloatInversePi = F32(1.0 / 3.14159265358979323846); ///< Constant float 1 / PI
+static const F32 FloatHalfPi = F32(0.5 * 3.14159265358979323846);    ///< Constant float 1/2 * PI
+static const F32 Float2InversePi = F32(2.0 / 3.14159265358979323846);///< Constant float 2 / PI
+static const F32 FloatInverse2Pi = F32(0.5 / 3.14159265358979323846);///< Constant float 2 / PI
+
+static const F32 FloatSqrt2 = F32(1.41421356237309504880f);          ///< Constant float sqrt(2)
+static const F32 FloatSqrtHalf = F32(0.7071067811865475244008443f);  ///< Constant float sqrt(0.5)
+
+static const S8  S8_MIN  = S8(-128);                              ///< Constant Min Limit S8
+static const S8  S8_MAX  = S8(127);                               ///< Constant Max Limit S8
+static const U8  U8_MAX  = U8(0xFF);                              ///< Constant Max Limit U8
+
+static const S16 S16_MIN = S16(-32768);                           ///< Constant Min Limit S16
+static const S16 S16_MAX = S16(32767);                            ///< Constant Max Limit S16
+static const U16 U16_MAX = U16(0xFFFF);                           ///< Constant Max Limit U16
+
+static const S32 S32_MIN = S32(-2147483647 - 1);                  ///< Constant Min Limit S32
+static const S32 S32_MAX = S32(2147483647);                       ///< Constant Max Limit S32
+static const U32 U32_MAX = U32(0xFFFFFFFF);                       ///< Constant Max Limit U32
+
+static const F32 F32_MIN = F32(1.175494351e-38F);                 ///< Constant Min Limit F32
+static const F32 F32_MAX = F32(3.402823466e+38F);                 ///< Constant Max Limit F32
+
+static const U64 U64_MAX = U64(0xFFFFFFFFFFFFFFFF);               ///< Constant Max Limit U64
+
+
+
 
 //----------------------------------------------------------------------------------
 // Identify the target Operating System
