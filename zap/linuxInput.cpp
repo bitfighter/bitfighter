a7a8fa4e1b02828f9a2e69b1f0d4e368cb45f52e
raptor <buckyballreaction@gmail.com>
2011-02-13 08:05:55 -0700

    whitespace cleanup in linuxInput

--- zap/linuxInput.cpp
+++ zap/linuxInput.cpp
@@ -54,15 +54,15 @@ const U32 MAX_BUTTON = 32;	// Can fit 32 buttons in a 32 bit integer!
 
 
 struct padData {
-	unsigned char axisCount;
-	unsigned char buttonCount;
-	int fd;
-	int version;
-	char devName[80];
-	js_event ev;
-	bool changed;
-	int aPos[MAX_AXIS];
-	int bPos[MAX_BUTTON];
+   unsigned char axisCount;
+   unsigned char buttonCount;
+   int fd;
+   int version;
+   char devName[80];
+   js_event ev;
+   bool changed;
+   int aPos[MAX_AXIS];
+   int bPos[MAX_BUTTON];
 };
 
 const int MAX_JOYPADS = 8;
@@ -84,13 +84,13 @@ void getModifierState(bool &shiftDown, bool &controlDown, bool &altDown)
 
    // This can most definitely be simplified
    altDown = (((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Alt_L) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Alt_L) & 7)) & 1) ||
-      ((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Alt_R) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Alt_R) & 7)) & 1));
+         ((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Alt_R) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Alt_R) & 7)) & 1));
 
    shiftDown = (((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Shift_L) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Shift_L) & 7)) & 1) ||
-      ((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Shift_R) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Shift_R) & 7)) & 1));
+         ((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Shift_R) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Shift_R) & 7)) & 1));
 
    controlDown = (((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Control_L) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Control_L) & 7)) & 1) ||
-      ((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Control_R) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Control_R) & 7)) & 1));
+         ((key_map_stat[XKeysymToKeycode(Xdisplay,XK_Control_R) >> 3] >> (XKeysymToKeycode(Xdisplay,XK_Control_R) & 7)) & 1));
 #endif
 }
 
@@ -99,134 +99,134 @@ void getModifierState(bool &shiftDown, bool &controlDown, bool &altDown)
 void InitJoystick()
 {
 #ifndef ZAP_DEDICATED
- //logprintf("finding joystick");
- if(gJoystickInit)
-    ShutdownJoystick();
+   //logprintf("finding joystick");
+   if(gJoystickInit)
+      ShutdownJoystick();
 
- char chr1[15] = "/dev/input/js0";
- gSticksFound = 0;
- gJoystickNames.clear();
- for(int i = 0; i < MAX_JOYPADS; i++)
- {
-   chr1[13] = i + '0';
-   joyPads[gSticksFound].fd = open(chr1, O_RDONLY);
-   if (joyPads[gSticksFound].fd != 0){
-      // Get pad info ...
-      //logprintf("Joystick found");
-      ioctl(joyPads[gSticksFound].fd, JSIOCGAXES,&joyPads[gSticksFound].axisCount);
-      ioctl(joyPads[gSticksFound].fd, JSIOCGBUTTONS, &joyPads[gSticksFound].buttonCount);
-      ioctl(joyPads[gSticksFound].fd, JSIOCGVERSION, &joyPads[gSticksFound].version);
-      ioctl(joyPads[gSticksFound].fd, JSIOCGNAME(80), &joyPads[gSticksFound].devName);
-      fcntl(joyPads[gSticksFound].fd, F_SETFL, O_NONBLOCK);
+   char chr1[15] = "/dev/input/js0";
+   gSticksFound = 0;
+   gJoystickNames.clear();
+   for(int i = 0; i < MAX_JOYPADS; i++)
+   {
+      chr1[13] = i + '0';
+      joyPads[gSticksFound].fd = open(chr1, O_RDONLY);
+      if (joyPads[gSticksFound].fd != 0){
+         // Get pad info ...
+         //logprintf("Joystick found");
+         ioctl(joyPads[gSticksFound].fd, JSIOCGAXES,&joyPads[gSticksFound].axisCount);
+         ioctl(joyPads[gSticksFound].fd, JSIOCGBUTTONS, &joyPads[gSticksFound].buttonCount);
+         ioctl(joyPads[gSticksFound].fd, JSIOCGVERSION, &joyPads[gSticksFound].version);
+         ioctl(joyPads[gSticksFound].fd, JSIOCGNAME(80), &joyPads[gSticksFound].devName);
+         fcntl(joyPads[gSticksFound].fd, F_SETFL, O_NONBLOCK);
 
-      //logprintf ("axis : %d\n", joyPads[gSticksFound].axisCount);
-      //logprintf ("buttons : %d\n", joyPads[gSticksFound].buttonCount);
-      //logprintf ("version : %d\n", joyPads[gSticksFound].version);
-      //logprintf ("name : %s\n", joyPads[gSticksFound].devName);
-      // set default values
-      joyPads[gSticksFound].changed = false;
-      for (int i=0;i<MAX_AXIS;i++) {joyPads[gSticksFound].aPos[i]=0;}
-      for (int i=0;i<MAX_BUTTON;i++) {joyPads[gSticksFound].bPos[i]=0;}
-      if(joyPads[gSticksFound].axisCount != 0 || joyPads[gSticksFound].buttonCount != 0)
-      {
-         gJoystickNames.push_back(joyPads[gSticksFound].devName);
-         gSticksFound++;
+         //logprintf ("axis : %d\n", joyPads[gSticksFound].axisCount);
+         //logprintf ("buttons : %d\n", joyPads[gSticksFound].buttonCount);
+         //logprintf ("version : %d\n", joyPads[gSticksFound].version);
+         //logprintf ("name : %s\n", joyPads[gSticksFound].devName);
+         // set default values
+         joyPads[gSticksFound].changed = false;
+         for (int i=0;i<MAX_AXIS;i++) {joyPads[gSticksFound].aPos[i]=0;}
+         for (int i=0;i<MAX_BUTTON;i++) {joyPads[gSticksFound].bPos[i]=0;}
+         if(joyPads[gSticksFound].axisCount != 0 || joyPads[gSticksFound].buttonCount != 0)
+         {
+            gJoystickNames.push_back(joyPads[gSticksFound].devName);
+            gSticksFound++;
+         }
+         else
+            close(joyPads[gSticksFound].fd);
       }
-      else
-         close(joyPads[gSticksFound].fd);
    }
- }
- logprintf("Found %i joysticks", gSticksFound);
- gJoystickInit = true;
+   logprintf("Found %i joysticks", gSticksFound);
+   gJoystickInit = true;
 #endif
 }
 
 bool ReadJoystick(F32 axes[MaxJoystickAxes], U32 &buttonMask, U32 &hatMask)
 {
 #ifndef ZAP_DEDICATED
-  if(! gJoystickInit)
-	InitJoystick();
+   if(! gJoystickInit)
+      InitJoystick();
 
-  U32 useStick = gUseStickNumber - 1;
-  if(useStick >= gSticksFound)
-    return false;
+   U32 useStick = gUseStickNumber - 1;
+   if(useStick >= gSticksFound)
+      return false;
 
-  if(joyPads[useStick].fd == 0)
-	return false;
+   if(joyPads[useStick].fd == 0)
+      return false;
 
-  int result = read(joyPads[useStick].fd, &joyPads[useStick].ev, sizeof(joyPads[useStick].ev));
-  int loop1 = 0;
+   int result = read(joyPads[useStick].fd, &joyPads[useStick].ev, sizeof(joyPads[useStick].ev));
+   int loop1 = 0;
 
-  while (result!=0 && loop1 < 100)
-  {
-    U32 num;
-    loop1++;
-    switch (joyPads[useStick].ev.type)
-    {
-		case JS_EVENT_INIT:
-		case JS_EVENT_INIT | JS_EVENT_AXIS:
-		case JS_EVENT_INIT | JS_EVENT_BUTTON:
-		  break;
-		case JS_EVENT_AXIS:
-        num = joyPads[useStick].ev.number;
-		  if(num < MAX_AXIS)
-          joyPads[useStick].aPos[num] = joyPads[useStick].ev.value;
-		  joyPads[useStick].changed = true;
-		  break;
-		case JS_EVENT_BUTTON:
-        joyPads[useStick].ev.number;
-		  if(num < MAX_BUTTON)
-		    joyPads[useStick].bPos[num] = joyPads[useStick].ev.value;
-		  joyPads[useStick].changed = true;
-		break;
-		default:
-		  //logprintf ("Other event ? %d\nnumber=%d\nvalue=%d\n", pad1.ev.type,pad1.ev.number, pad1.ev.value);
-		  break;
-    }
-    result = read(joyPads[useStick].fd, &joyPads[useStick].ev, sizeof(joyPads[useStick].ev));
-  }
+   while (result!=0 && loop1 < 100)
+   {
+      U32 num;
+      loop1++;
+      switch (joyPads[useStick].ev.type)
+      {
+      case JS_EVENT_INIT:
+      case JS_EVENT_INIT | JS_EVENT_AXIS:
+      case JS_EVENT_INIT | JS_EVENT_BUTTON:
+      break;
+      case JS_EVENT_AXIS:
+         num = joyPads[useStick].ev.number;
+         if(num < MAX_AXIS)
+            joyPads[useStick].aPos[num] = joyPads[useStick].ev.value;
+         joyPads[useStick].changed = true;
+         break;
+      case JS_EVENT_BUTTON:
+         joyPads[useStick].ev.number;
+         if(num < MAX_BUTTON)
+            joyPads[useStick].bPos[num] = joyPads[useStick].ev.value;
+         joyPads[useStick].changed = true;
+         break;
+      default:
+         //logprintf ("Other event ? %d\nnumber=%d\nvalue=%d\n", pad1.ev.type,pad1.ev.number, pad1.ev.value);
+         break;
+      }
+      result = read(joyPads[useStick].fd, &joyPads[useStick].ev, sizeof(joyPads[useStick].ev));
+   }
 
-  buttonMask = 0;
-  for(S32 b=0; b<joyPads[useStick].buttonCount && b < MAX_BUTTON; b++)
-    buttonMask = buttonMask | ((joyPads[useStick].bPos[b] != 0 ? 1 : 0) << b);
+   buttonMask = 0;
+   for(S32 b=0; b<joyPads[useStick].buttonCount && b < MAX_BUTTON; b++)
+      buttonMask = buttonMask | ((joyPads[useStick].bPos[b] != 0 ? 1 : 0) << b);
 
-  for(S32 b=0; b<MaxJoystickAxes && b<MAX_AXIS; b++)
-    axes[b] = F32(joyPads[useStick].aPos[b])/32768; //convert to Float between -1.0 and 1.0
+   for(S32 b=0; b<MaxJoystickAxes && b<MAX_AXIS; b++)
+      axes[b] = F32(joyPads[useStick].aPos[b])/32768; //convert to Float between -1.0 and 1.0
 
-  hatMask = 0; //POV (d-pad) either get maps to 2 more axes (logitech dual action), or 4 more buttons?
+   hatMask = 0; //POV (d-pad) either get maps to 2 more axes (logitech dual action), or 4 more buttons?
 
-  return true;
+   return true;
 #else
-  return false;
+   return false;
 #endif
 }
 
 const char *GetJoystickName()
 {
 #ifndef ZAP_DEDICATED
-  if(! gJoystickInit)
-	InitJoystick();
+   if(! gJoystickInit)
+      InitJoystick();
 
-  U32 useStick = gUseStickNumber - 1;
-  if(useStick >= gSticksFound)
-    return "";
+   U32 useStick = gUseStickNumber - 1;
+   if(useStick >= gSticksFound)
+      return "";
 
-  return joyPads[useStick].devName;
+   return joyPads[useStick].devName;
 #else
-  return "";
+   return "";
 #endif
 }
 
 void ShutdownJoystick()
 {
 #ifndef ZAP_DEDICATED
-  for(int i = 0; i < MAX_JOYPADS; i++)
-  {
-    if(joyPads[i].fd != 0) close(joyPads[i].fd);
-  }
-  gSticksFound = 0;
-  gJoystickNames.clear();
-  gJoystickInit = false;
+   for(int i = 0; i < MAX_JOYPADS; i++)
+   {
+      if(joyPads[i].fd != 0) close(joyPads[i].fd);
+   }
+   gSticksFound = 0;
+   gJoystickNames.clear();
+   gJoystickInit = false;
 #endif
 }
 
