2ba863f42c68dc47fae75bebe97f750d473b4729
sam8641 <sam8641@gmail.com>
2011-02-22 17:19:37 -0600

    No more hostile or neutral bots - creates problems with game and spawn points.

--- zap/gameType.cpp
+++ zap/gameType.cpp
@@ -1736,7 +1736,7 @@ void GameType::countTeamPlayers()
 
    for(S32 i = 0; i < mClientList.size(); i++)
    {
-      if(mClientList[i]->getTeam() >= -2 && mClientList[i]->getTeam() < mTeams.size())
+      if(mClientList[i]->getTeam() >= 0 && mClientList[i]->getTeam() < mTeams.size())
       { // robot could be neutral or hostile, skip out of range team numbers.
          if(mClientList[i]->isRobot)
             mTeams[mClientList[i]->getTeam()].numBots++;
@@ -1796,7 +1796,7 @@ void GameType::serverAddClient(GameConnection *theClient)
       Ship *ship = dynamic_cast<Ship *>(theClient->getControlObject());
       if(ship)
       {
-         if(ship->getTeam() >= -99999 && ship->getTeam() < mTeams.size())
+         if(ship->getTeam() >= 0 && ship->getTeam() < mTeams.size()) // no more neutral or hostile bots
             minTeamIndex = ship->getTeam();
       }
       ship->setMaskBits(Ship::ChangeTeamMask);  // This is needed to avoid gray robot ships when using /addbot
