43871c5af3ed641cc6ecdc1f0c1d6f50e0539bd6
sam8641 <sam8641@gmail.com>
2011-03-22 01:56:38 -0500

    Fix hundreds of compile warnings.

--- poly2tri/sweep/sweep_context.h
+++ poly2tri/sweep/sweep_context.h
@@ -157,7 +157,7 @@ inline AdvancingFront* SweepContext::front()
 
 inline int SweepContext::point_count()
 {
-  return points_.size();
+  return (int)points_.size();
 }
 
 inline void SweepContext::set_head(Point* p1)
--- zap/barrier.cpp
+++ zap/barrier.cpp
@@ -134,8 +134,7 @@ Barrier::Barrier(const Vector<Point> &points, F32 width, bool solid)
          return;
       }
 
-      mBotZoneBufferGeometry = mPoints;  /// buffer doesn't work on reverse order points of polygon
-      //bufferPolyWallForBotZone(mPoints, mBotZoneBufferGeometry);
+      bufferPolyWallForBotZone(mPoints, mBotZoneBufferGeometry);
    }
    else
    {
