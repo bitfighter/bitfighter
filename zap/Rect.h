248cd9839f925d0eb50825f31a2817cfc66b7c49
eykamp <chris@eykamp.com>
2011-10-31 00:54:37 +0100

    Line the comments up nice and straight-like

--- zap/Rect.h
+++ zap/Rect.h
@@ -46,12 +46,12 @@ public:
    typedef float member_type;
    Point min, max;
 
-   Rect();                                     // Constuctor
-   Rect(const Point &p1, const Point &p2);    // Constuctor
-   Rect(F32 x1, F32 y1, F32 x2, F32 y2);       // Constuctor
-   Rect(const Point &p, member_type size);    // Constuctor, takes point and "radius"
+   Rect();                                      // Constuctor
+   Rect(const Point &p1, const Point &p2);      // Constuctor
+   Rect(F32 x1, F32 y1, F32 x2, F32 y2);        // Constuctor
+   Rect(const Point &p, member_type size);      // Constuctor, takes point and "radius"
 
-   Rect(const Vector<Point> &p);         // Construct as a bounding box around multiple points
+   Rect(const Vector<Point> &p);                // Construct as a bounding box around multiple points
 
    Point getCenter() const;
 
