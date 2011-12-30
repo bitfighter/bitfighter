f9596db5ed1ab9a1d0ed69fc20d607719ba7485f
eykamp <chris@eykamp.com>
2011-12-30 19:21:00 +0100

    Clean up demo plugin script

--- resource/levels/plugin_arc.lua
+++ resource/levels/plugin_arc.lua
@@ -1,77 +1,76 @@
--- Ugly script that should now be uber-powerful
--- mostly stolen from _k - thanks!
--- hacked into inogural plugin, probably making it uglier yet
+-- Sample Bitfighter editor plugin script
+-- Create rudimentary curved walls or zones
+-- Based on work by _k, adapted by raptor and watusimoto
 
--- Defaults
-ItemType = "BarrierMaker"
-BarrierWidth = 50
-CenterX = 0
-CenterY = 0
-radius = 1
-degrees = 90
-StartPoint = 0
-precision = 30
-
-
-function getArgsMenu()  -- Returns a table of strings representing the cmd line args
+--
+-- If this function exists, it should return a list of menu items that can be used to build an options menu
+-- If this function is not implemented, or returns nil, the menu will not be displayed, and main() will be
+-- run without args
+--
+-- For a full list of the menu widgets available, see the Bitfighter wiki on bitfighter.org
+--
+function getArgsMenu()
    
-   return "Create Arc",	     -- Title shown on menu
+   return "Create Arc",         -- Title shown on menu
       {
-         CounterMenuItem:new("Angle",          90, 1,     0,  360, "deg.",       "", "Sweep of arc"),    
-         CounterMenuItem:new("Points",      64, 1,     4,  512, "points",     "", "Number of points to complete a full circle"),
-         CounterMenuItem:new("Radius of arc", 100, 1,     1,  500, "grid units", "", "Radius of the arc"),
-         CounterMenuItem:new("Start of arc",   90, 1,     0,  360, "degrees",    "", "Start angle of arc from the positive x axis"),
+         CounterMenuItem:new("Angle",          90, 1,     0,  360, "deg.",        "", "Sweep of arc"),    
+         CounterMenuItem:new("Points",         64, 1,     4,  512, "points",      "", "Number of points to complete a full circle"),
+         CounterMenuItem:new("Radius of arc", 100, 1,     1,  500, "grid units",  "", "Radius of the arc"),
+         CounterMenuItem:new("Start of arc",   90, 1,     0,  360, "degrees",     "", "Start angle of arc from the positive x axis"),
          ToggleMenuItem:new ("Type", { "BarrierMaker", "LoadoutZone", "GoalZone" }, 1, true, "Type of item to insert"),
          CounterMenuItem:new("Barrier Width",  50, 1,     1,   50,  "grid units", "", "With of wall if BarrierMaker is selected above"),
-         CounterMenuItem:new("Center X",        0, 10, -10000, 10000,  "",           "", "X coordinate of center of arc"),
-         CounterMenuItem:new("Center Y",        0, 10, -10000, 10000,  "",           "", "Y coordinate of center of arc")
+         CounterMenuItem:new("Center X",        0, 10, -10000, 10000,  "",        "", "X coordinate of center of arc"),
+         CounterMenuItem:new("Center Y",        0, 10, -10000, 10000,  "",        "", "Y coordinate of center of arc")
       }
-
 end
 
 
--- Do the logic and build the level line
-
--- Quick helper to convert from degrees to radians
+--
+-- Helper to convert degrees to radians
+--
 function toRads(deg)
-   return (deg * 2 * math.pi)/360
+   return (deg * math.pi) / 180
 end
 
 
+--
+-- The main body of the code gets put in main()
+--
 function main()
    -- arg table will include values from menu items above, in order
-  
+ 
    local gridsize = levelgen:getGridSize()
    
-   scriptName = arg[0]
-   degrees = arg[1]
-   precision = arg[2]
-   radius = arg[3] / gridsize
-   StartPoint = arg[4]
-   ItemType = arg[5]
-   BarrierWidth = arg[6]
-   CenterX = arg[7] / gridsize
-   CenterY = arg[8] / gridsize
+   local scriptName = arg[0]
+   local degrees = arg[1]
+   local precision = arg[2]
+   local radius = arg[3] / gridsize
+   local startPoint = arg[4]
+   local itemType = arg[5]
+   local barrierWidth = arg[6]
+   local centerX = arg[7] / gridsize
+   local centerY = arg[8] / gridsize
    
    
-   local Curve
+   local levelLine
 
-   if(ItemType == "BarrierMaker") then
-       Curve = ItemType .. " " .. BarrierWidth .. " "
+   -- First part of the level line
+   if(itemType == "BarrierMaker") then
+       levelLine = itemType .. " " .. barrierWidth .. " "
    else
-       Curve = ItemType .. " 0 "
+       levelLine = itemType .. " 0 "
    end
-
-   for x = toRads(StartPoint), toRads(degrees) + toRads(StartPoint),  2 * math.pi / precision do
-      Curve = Curve .. radius * math.cos(x) + CenterX .. " " .. radius * math.sin(x) - CenterY .. "  "
+   
+   -- Now add some coordinates
+   for x = toRads(startPoint), toRads(degrees) + toRads(startPoint), 2 * math.pi / precision do
+      levelLine = levelLine .. radius * math.cos(x) + centerX .. " " .. radius * math.sin(x) - centerY .. "  "
    end
 
    if(degrees == 360) then
-      Curve = Curve .. radius * math.cos(toRads(StartPoint)) + CenterX .. " " .. radius * math.sin(toRads(StartPoint)) - CenterY .. "  "
+      levelLine = levelLine .. radius * math.cos(toRads(startPoint)) + centerX .. " " .. radius * math.sin(toRads(startPoint)) - centerY .. "  "
    end
 
-   print(Curve)  -- prints to console
-
-   levelgen:addLevelLine(Curve)
+   -- Now add item to the level
+   levelgen:addLevelLine(levelLine)
 end   
 
