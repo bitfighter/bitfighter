0807244c27fa5781e730f5bbcb34e02e0ec1f3e2
raptor <buckyballreaction@gmail.com>
2012-01-20 00:15:59 -0700

    Fix compiling with older gcc

--- zap/game.cpp
+++ zap/game.cpp
@@ -2093,7 +2093,7 @@ void ServerGame::cycleLevel(S32 nextLevel)
 
          clientInfo->setScore(0); // Reset player scores, for non team game types
       }
-      delete mGameType;  // need to delete old GameType..
+      delete mGameType.getPointer();  // need to delete old GameType..
    }
 	
    setCurrentLevelIndex(nextLevel, getPlayerCount());
