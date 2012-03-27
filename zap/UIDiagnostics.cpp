0d5a337c6ea8c718eb2cb0df72a1ea334978005a
raptor <buckyballreaction@gmail.com>
2012-03-27 13:58:57 -0600

    Show test badges on diagnostics only for debug builds

--- zap/UIDiagnostics.cpp
+++ zap/UIDiagnostics.cpp
@@ -610,7 +610,7 @@ void DiagnosticUserInterface::render()
 
 
       // Temporary placeholder for badge testing -- centered at xpos, ypos, with radius of rad (i.e. 2 x rad across)
-
+#ifdef TNL_DEBUG
       for(S32 i = 0; i < 2; i++)
       {
          F32 x;
@@ -709,7 +709,7 @@ void DiagnosticUserInterface::render()
          glPopMatrix();
 
       }
-
+#endif // TNL_DEBUG
    }
 }
 
