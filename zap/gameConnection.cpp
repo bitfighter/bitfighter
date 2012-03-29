98d582e55eed15e47f6fc407692bad8ae891aea8
eykamp <chris@eykamp.com>
2012-03-29 02:42:27 +0200

    Fix merge problem

--- zap/gameConnection.cpp
+++ zap/gameConnection.cpp
@@ -1165,22 +1165,22 @@ TNL_IMPLEMENT_RPC(GameConnection, s2cCancelShutdown, (), (), NetClassGroupGameMa
 }
 
 
-// Server tells clients that another player is idle and will not be joining us for the moment
-TNL_IMPLEMENT_RPC(GameConnection, s2cSetIsSpawnDelayed, (StringTableEntry name, bool isDelayed), (name, isDelayed), 
-                  NetClassGroupGameMask, RPCGuaranteed, RPCDirServerToClient, 0)
-{
-#ifndef ZAP_DEDICATED
-   ClientInfo *clientInfo = mClientGame->findClientInfo(name);
-
-   // Might not find clientInfo if level just cycled and players haven't been re-sent to client yet.  In which case,
-   // this is ok, since spawn-delay status will be sent with s2cAddClient().
-
-   if(!clientInfo)      
-      return;
-
-   clientInfo->setSpawnDelayed(NULL, isDelayed);     // Calls remoteClientInfo version
-#endif
-}
+//// Server tells clients that another player is idle and will not be joining us for the moment
+//TNL_IMPLEMENT_RPC(GameConnection, s2cSetIsSpawnDelayed, (StringTableEntry name, bool isDelayed), (name, isDelayed), 
+//                  NetClassGroupGameMask, RPCGuaranteed, RPCDirServerToClient, 0)
+//{
+//#ifndef ZAP_DEDICATED
+//   ClientInfo *clientInfo = mClientGame->findClientInfo(name);
+//
+//   // Might not find clientInfo if level just cycled and players haven't been re-sent to client yet.  In which case,
+//   // this is ok, since spawn-delay status will be sent with s2cAddClient().
+//
+//   if(!clientInfo)      
+//      return;
+//
+//   clientInfo->setSpawnDelayed(NULL, isDelayed);     // Calls remoteClientInfo version
+//#endif
+//}
 
 
 // Server tells clients that another player is idle and will not be joining us for the moment
