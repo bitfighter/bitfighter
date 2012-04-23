2680fbee6229c8fd3558360fd0a3d3f18c72a504
raptor <buckyballreaction@gmail.com>
2012-04-22 19:09:32 -0600

    Revert BirdOfPrey ship design and add flames; disable bots using new shapes for now

--- zap/robot.cpp
+++ zap/robot.cpp
@@ -1019,7 +1019,7 @@ Robot::Robot() : Ship(NULL, TEAM_NEUTRAL, Point(), 1, true),
       mModuleSecondaryActive[i] = false;
    }
 #ifndef ZAP_DEDICATED
-   mShapeType = ShipShape::Cube;
+   mShapeType = ShipShape::Normal;
 #endif
 }
 
