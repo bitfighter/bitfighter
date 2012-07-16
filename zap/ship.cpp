143a0a1a010c618972a1d9b74cf8f9efc42da69d
sam8641 <sam8641@gmail.com>
2012-07-16 14:10:10 -0500

    Save small CPU usage for non-moving ships, Fix inconsistant line ending.

--- zap/ship.cpp
+++ zap/ship.cpp
@@ -330,6 +330,10 @@ void Ship::processMove(U32 stateIndex)
    const F32 ARMOR_SPEED_PENALTY_FACT = 1;
 
    copyMoveState(stateIndex, LastProcessState);
+   setAngle(stateIndex, mCurrentMove.angle);
+
+   if(mCurrentMove.x == 0 && mCurrentMove.y == 0 && getVel(stateIndex) == (const Point(0,0)))
+      return;  // saves small amount of CPU processing to not processing any of below when ship is not moving.
 
    F32 maxVel = (isModulePrimaryActive(ModuleBoost) ? BoostMaxVelocity : MaxVelocity) *
                 (hasModule(ModuleArmor) ? ARMOR_SPEED_PENALTY_FACT : 1);
@@ -366,7 +370,6 @@ void Ship::processMove(U32 stateIndex)
    else
       setVel(stateIndex, requestVel);
 
-   setAngle(stateIndex, mCurrentMove.angle);
    move(time, stateIndex, false);
 }
 
