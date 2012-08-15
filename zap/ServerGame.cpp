9f163f4c7b81edf2bf7e3e5c0b360320daab4651
eykamp <chris@eykamp.com>
2012-08-15 20:45:02 +0200

    Formatting

--- zap/ServerGame.cpp
+++ zap/ServerGame.cpp
@@ -1046,9 +1046,9 @@ void ServerGame::runLevelGenScript(const string &scriptName)
       return;
    }
 
-   // The script file will be the first argument, subsequent args will be passed on to the script -- will be deleted when level ends in ServerGame::cleanUp()
-   LuaLevelGenerator *levelgen = new LuaLevelGenerator(fullname, *getGameType()->getScriptArgs(), getGridSize(), 
-                                                       getGameObjDatabase(), this);
+   // The script file will be the first argument, subsequent args will be passed on to the script -- 
+   // will be deleted when level ends in ServerGame::cleanUp()
+   LuaLevelGenerator *levelgen = new LuaLevelGenerator(fullname, *getGameType()->getScriptArgs(), getGridSize(), getGameObjDatabase(), this);
 
    if(!levelgen->runScript())
       delete levelgen;
