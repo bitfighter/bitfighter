cc5026e80e373850ce061e5849ce191438ea311f
eykamp <chris@eykamp.com>
2012-09-13 23:52:19 +0200

    Remove unused code

--- luadoc.pl
+++ luadoc.pl
@@ -117,46 +117,6 @@ foreach my $file (@files) {
          # otherwise...
          push(@comments, $line);
       }
-
-
-      # # Convert Doxygen /** to Luadoc ---
-      # $line =~ s|^ */\*\* *$|---| && push(@keepers, $line) && next;  # s|^ */\*\* *$|---|p
-
-      # # Convert special tags to Lua expressions, which is what Luadoc can parse
-      # # Lines after @luafunc & @luamod will be ignored by Luadoc
-      # # Doxygen comments that do not contain any of these tags have no impact on the Luadoc output
-      #                                                                          # Original naev sed commands
-      # $line =~ s|^ *\* *\@luafunc|function|             && push(@keepers, $line) && next;  # s|^ *\* *@luafunc|function|p
-
-      # $line =~ s|^ *\* *\@luamod *(.*)|module "\1"|     && push(@keepers, $line) && next;  # s|^ *\* *@luamod *\(.*\)|module "\1"|p
-
-      # # Rename some tags:      
-      # $line =~ s|^ *\* *\@brief|-- \@description|       && push(@keepers, $line) && next;  # s|^ *\* *@brief|-- @description|p
-      # $line =~ s|^ *\* *\@luasee|-- \@see|              && push(@keepers, $line) && next;  # s|^ *\* *@luasee|-- @see|p
-      # $line =~ s|^ *\* *\@luaparam|-- \@param|          && push(@keepers, $line) && next;  # s|^ *\* *@luaparam|-- @param|p
-      # $line =~ s|^ *\* *\@luareturn|-- \@return|        && push(@keepers, $line) && next;  # 
-      # $line =~ s|^ *\* *\@usage|-- \@usage|             && push(@keepers, $line) && next;  # s|^ *\* *@usage|-- @usage|p
-      # $line =~ s|^ *\* *\@description|-- \@description| && push(@keepers, $line) && next;  # s|^ *\* *@description|-- @description|p
-      # $line =~ s|^ *\* *\@name|-- \@name|               && push(@keepers, $line) && next;  # s|^ *\* *@name|-- @name|p
-      # $line =~ s|^ *\* *\@class|-- \@class|             && push(@keepers, $line) && next;  # s|^ *\* *@class|-- @class|p
-      # $line =~ s|^ *\* *\@field|-- \@field|             && push(@keepers, $line) && next;  # s|^ *\* *@field|-- @field|p
-      # $line =~ s|^ *\* *\@release|-- \@release|         && push(@keepers, $line) && next;  # s|^ *\* *@release|-- @release|p
-       
-      # # Custom tags:
-      # $line =~ s|^ *\* *\@code|-- <pre>|                && push(@keepers, $line) && next;  # s|^ *\* *@code|-- <pre>|p
-      # $line =~ s|^ *\* *\@endcode|-- </pre>|            && push(@keepers, $line) && next;  # s|^ *\* *@endcode|-- </pre>|p
-    
-      # # Remove other tags:
-      # # $line =~ \|^ *\* *@.*|                          && push(@keepers, $line) && next;  # \|^ *\* *@.*|d
-    
-      # # Insert newline between comments, replace */ with \n:
-      # # $line =~ \|^ *\*/| && push(@keepers, $line) && next; # \|^ *\*/|c
-
-      # # Keep other comments, replace * with --
-      # $line =~ s|^ *\*|--|                              && push(@keepers, $line) && next; # s|^ *\*|--|p
-
-      # # Lines not matched above will not be copied into @keepers
-      # #push(@keepers, $line);
    }
 
    # If we added any lines to keepers, write it out... otherwise skip it!
