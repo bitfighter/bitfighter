7dc0d036f54d639341419f7082ff2505a237477c
raptor <buckyballreaction@gmail.com>
2012-09-18 16:12:03 -0600

    Fix compiling in Linux.  sam686, did you mean RefPtr here?

--- tnl/tnlNetInterface.h
+++ tnl/tnlNetInterface.h
@@ -205,7 +205,7 @@ protected:
       Address remoteAddress;       ///< The address to send this packet to.
       U32 sendTime;                ///< Time when we should send the packet.
       U32 packetSize;              ///< Size, in bytes, of the packet data.
-      SafePtr<NetConnection> receiveTo; // Used if delayed receiving
+      RefPtr<NetConnection> receiveTo; // Used if delayed receiving
       bool isReceive;
       U8 packetData[1];            ///< Packet data.
    };
