d292865360b67c22fba642236c20c24c881b85d0
eykamp <chris@eykamp.com>
2012-09-19 22:48:43 +0200

    Kill a magic number, restore seeker color

--- zap/gameObjectRender.cpp
+++ zap/gameObjectRender.cpp
@@ -1417,7 +1417,7 @@ void renderHeatSeeker(const Point &pos, F32 angleRadians, F32 speed, U32 timeRem
       glRotatef(angleRadians * 360.f / FloatTau, 0, 0, 1.0);
 
       // The flames first!
-      F32 speedRatio = speed / 600.f;  // 600 is the current projectile velocity for seeker
+      F32 speedRatio = speed / GameWeapon::weaponInfo[WeaponHeatSeeker].projVelocity;  
       glColor(Colors::yellow, 0.5);
       F32 innerFlame[] = {
             -8, -1,
@@ -1434,7 +1434,7 @@ void renderHeatSeeker(const Point &pos, F32 angleRadians, F32 speed, U32 timeRem
       renderVertexArray(outerFlame, 3, GL_LINE_STRIP);
 
       // The body of the seeker
-      glColor3f(1.0, 0, 0.45);  // A redder magenta
+      glColor3f(1.0, 0, 0.35);  // A redder magenta
       F32 vertices[] = {
             -8, -4,
             -8,  4,
