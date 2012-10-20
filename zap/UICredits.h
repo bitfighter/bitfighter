1eb8e0bfc5be4e34cbb4ce934bacadb754989c49
eykamp <chris@eykamp.com>
2012-10-20 22:01:14 +0200

    Formatting

--- zap/UICredits.h
+++ zap/UICredits.h
@@ -80,6 +80,9 @@ public:
    void render();
 };
 
+////////////////////////////////////////
+////////////////////////////////////////
+
 // Credits UI
 class CreditsUserInterface : public UserInterface
 {
