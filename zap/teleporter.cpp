b13a8df946d709bcab6dd3c05e4344d4af4532d8
eykamp <chris@eykamp.com>
2012-11-11 14:37:41 +0100

    Now that we've clarified that TELEPORTER_RADIUS is indeed a radius, and that the Rect(point, size) constructor takes a diameter and not a radius, it becomes plain to see why teleporters disappear before they are completely off the screen.  And once we understand the cause, the fix is trivial, and is the main focus of this checkin, along with finalizing the removal of yet another relic of this confusion.  All of these problems could have been avoided with better variable names and clearer comments (or, really, any comments at all).

--- zap/teleporter.cpp
+++ zap/teleporter.cpp
@@ -540,7 +540,7 @@ bool Teleporter::collide(BfObject *otherObject)
 bool Teleporter::getCollisionCircle(U32 state, Point &center, F32 &radius) const
 {
    center = getVert(0);
-   radius = TELEPORTER_RADIUS;// / 2;
+   radius = TELEPORTER_RADIUS;
    return true;
 }
 
@@ -553,7 +553,7 @@ bool Teleporter::getCollisionPoly(Vector<Point> &polyPoints) const
 
 void Teleporter::computeExtent()
 {
-   setExtent(Rect(getVert(0), (F32)TELEPORTER_RADIUS));
+   setExtent(Rect(getVert(0), (F32)TELEPORTER_RADIUS * 2));
 }
 
 
