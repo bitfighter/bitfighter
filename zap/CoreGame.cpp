3aa862ebbc1126e0d6bd41984575150809c6b0c7
eykamp <chris@eykamp.com>
2012-12-27 21:51:36 +0100

    Make cores rotate again; still can't kill panels

--- zap/CoreGame.cpp
+++ zap/CoreGame.cpp
@@ -815,6 +815,7 @@ Vector<Point> CoreItem::getRepairLocations(const Point &repairOrigin)
 void CoreItem::onAddedToGame(Game *theGame)
 {
    Parent::onAddedToGame(theGame);
+    linkToIdleList(&theGame->idlingObjects);
 
    // Make cores always visible
    if(!isGhost())
