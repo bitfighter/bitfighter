00127879ed0654ae18c7aec659ca2ec7a0c4b4c0
eykamp <chris@eykamp.com>
2012-12-30 12:27:35 +0100

    Formatting

--- zap/InputCode.cpp
+++ zap/InputCode.cpp
@@ -977,16 +977,16 @@ InputCode InputCodeManager::sdlKeyToInputCode(int key)
 
 // Numpad keys were renamed in SDL2
 #if SDL_VERSION_ATLEAST(2,0,0)
-#define SDLK_KP0 SDLK_KP_0
-#define SDLK_KP1 SDLK_KP_1
-#define SDLK_KP2 SDLK_KP_2
-#define SDLK_KP3 SDLK_KP_3
-#define SDLK_KP4 SDLK_KP_4
-#define SDLK_KP5 SDLK_KP_5
-#define SDLK_KP6 SDLK_KP_6
-#define SDLK_KP7 SDLK_KP_7
-#define SDLK_KP8 SDLK_KP_8
-#define SDLK_KP9 SDLK_KP_9
+#  define SDLK_KP0 SDLK_KP_0
+#  define SDLK_KP1 SDLK_KP_1
+#  define SDLK_KP2 SDLK_KP_2
+#  define SDLK_KP3 SDLK_KP_3
+#  define SDLK_KP4 SDLK_KP_4
+#  define SDLK_KP5 SDLK_KP_5
+#  define SDLK_KP6 SDLK_KP_6
+#  define SDLK_KP7 SDLK_KP_7
+#  define SDLK_KP8 SDLK_KP_8
+#  define SDLK_KP9 SDLK_KP_9
 #endif
 
 	   // Numeric keypad
