e09e26d1253f093b8d150c5e563fcb758c437ae5
eykamp <chris@eykamp.com>
2013-04-21 14:44:14 +0200

    Formatting

--- zap/gameLoader.cpp
+++ zap/gameLoader.cpp
@@ -24,7 +24,7 @@
 //------------------------------------------------------------------------------------
 
 #ifdef _MSC_VER
-#pragma warning (disable: 4996)     // Disable POSIX deprecation, certain security warnings that seem to be specific to VC++
+#  pragma warning (disable: 4996)     // Disable POSIX deprecation, certain security warnings that seem to be specific to VC++
 #endif
 
 #include "gameLoader.h"
