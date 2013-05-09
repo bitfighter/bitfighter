35984a47c1402c582ac82256cc4079e4f71262dc
eykamp <chris@eykamp.com>
2013-05-09 23:52:56 +0200

    Suppress rebuilding zones when quitting testing from the editor

--- zap/gameType.cpp
+++ zap/gameType.cpp
@@ -2374,7 +2374,8 @@ void GameType::serverRemoveClient(ClientInfo *clientInfo)
 
    // Note that we do not need to delete clientConnection... TNL handles that, and the destructor gets runs shortly after we get here
 
-   static_cast<ServerGame *>(getGame())->suspendIfNoActivePlayers();
+   if(!static_cast<ServerGame *>(getGame())->isTestServer())
+      static_cast<ServerGame *>(getGame())->suspendIfNoActivePlayers();
 }
 
 
