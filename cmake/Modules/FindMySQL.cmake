bf7b683eef451bf5d3cb073813705aa9e21ead9a
raptor <buckyballreaction@gmail.com>
2013-05-14 10:31:25 -0600

    Make FindMySQL cmake module more robust

--- cmake/Modules/FindMySQL.cmake
+++ cmake/Modules/FindMySQL.cmake
@@ -5,43 +5,40 @@
 #  MYSQL_LIBRARIES   - List of libraries when using MySQL.
 #  MYSQL_FOUND       - True if MySQL found.
 
-IF (MYSQL_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(MYSQL_FIND_QUIETLY TRUE)
-ENDIF (MYSQL_INCLUDE_DIR)
-
-FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
-  /usr/local/include/mysql
-  /usr/include/mysql
+set(MYSQL_SEARCH_PATHS
+	${MYSQL_SEARCH_PATHS}
+	~/Library/Frameworks
+	/Library/Frameworks
+	/usr/local
+	/usr
+	/sw        # Fink
+	/opt/local # DarwinPorts
+	/opt/csw   # Blastwave
+	/opt
 )
 
-SET(MYSQL_NAMES mysqlclient mysqlclient_r)
-FIND_LIBRARY(MYSQL_LIBRARY
-  NAMES ${MYSQL_NAMES}
-  PATHS /usr/lib /usr/local/lib
-  PATH_SUFFIXES mysql
+find_path(MYSQL_INCLUDE_DIR 
+	NAMES mysql.h
+	HINTS ENV MYSQLDIR
+	PATH_SUFFIXES include include/mysql
+	PATHS ${MYSQL_SEARCH_PATHS}
 )
 
-IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-  SET(MYSQL_FOUND TRUE)
-  SET( MYSQL_LIBRARIES ${MYSQL_LIBRARY} )
-ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-  SET(MYSQL_FOUND FALSE)
-  SET( MYSQL_LIBRARIES )
-ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+find_library(MYSQL_LIBRARY
+	NAMES libmysqlclient mysqlclient libmysqlclient_r mysqlclient_r
+	HINTS ENV MYSQLDIR
+	PATH_SUFFIXES lib64 lib libs64 libs libs/Win32 libs/Win64
+	PATHS ${MYSQL_SEARCH_PATHS}
+)
 
-IF (MYSQL_FOUND)
-  IF (NOT MYSQL_FIND_QUIETLY)
-    MESSAGE(STATUS "Found MySQL: ${MYSQL_LIBRARY}")
-  ENDIF (NOT MYSQL_FIND_QUIETLY)
-ELSE (MYSQL_FOUND)
-  IF (MYSQL_FIND_REQUIRED)
-    MESSAGE(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
-    MESSAGE(FATAL_ERROR "Could NOT find MySQL library")
-  ENDIF (MYSQL_FIND_REQUIRED)
-ENDIF (MYSQL_FOUND)
 
-MARK_AS_ADVANCED(
-  MYSQL_LIBRARY
-  MYSQL_INCLUDE_DIR
-  )
+if(MYSQL_LIBRARY)
+	set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+else()
+	set(MYSQL_LIBRARIES)
+endif()
+
+
+# Handle the QUIETLY and REQUIRED arguments and set XXX_FOUND to TRUE if all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(MYSQL DEFAULT_MSG MYSQL_LIBRARIES MYSQL_INCLUDE_DIR)
