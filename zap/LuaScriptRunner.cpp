da1d533bf63718bf22df1b40ebb0bac3539897aa
raptor <buckyballreaction@gmail.com>
2013-06-05 01:08:54 -0600

    We have our Lua sandbox!

--- zap/LuaScriptRunner.cpp
+++ zap/LuaScriptRunner.cpp
@@ -453,6 +453,47 @@ void LuaScriptRunner::deleteScript(const char *name)
 }
 
 
+// This is our sandboxed Lua environment that gets set as the global environment for any
+// user script that is run.
+//
+// This is basically a whitelist of Lua functions known to be safe, taken from here:
+//    http://lua-users.org/wiki/SandBoxes
+static const char *sandbox_env =
+      "sandbox_e = {"
+      "  ipairs = ipairs,"
+      "  next = next,"
+      "  pairs = pairs,"
+      "  pcall = pcall,"
+      "  select = select,"
+      "  tonumber = tonumber,"
+      "  tostring = tostring,"
+      "  type = type,"
+      "  unpack = unpack,"
+      "  coroutine = { create = coroutine.create, resume = coroutine.resume,"
+      "     running = coroutine.running, status = coroutine.status, "
+      "     wrap = coroutine.wrap },"
+      "  string = { byte = string.byte, char = string.char, find = string.find,"
+      "     format = string.format, gmatch = string.gmatch, gsub = string.gsub,"
+      "     len = string.len, lower = string.lower, match = string.match,"
+      "     rep = string.rep, reverse = string.reverse, sub = string.sub,"
+      "     upper = string.upper },"
+      "  table = { insert = table.insert, maxn = table.maxn, remove = table.remove,"
+      "     sort = table.sort },"
+      "  math = { abs = math.abs, acos = math.acos, asin = math.asin,"
+      "     atan = math.atan, atan2 = math.atan2, ceil = math.ceil, cos = math.cos,"
+      "     cosh = math.cosh, deg = math.deg, exp = math.exp, floor = math.floor,"
+      "     fmod = math.fmod, frexp = math.frexp, huge = math.huge,"
+      "     ldexp = math.ldexp, log = math.log, log10 = math.log10, max = math.max,"
+      "     min = math.min, modf = math.modf, pi = math.pi, pow = math.pow,"
+      "     rad = math.rad, sin = math.sin, sinh = math.sinh, sqrt = math.sqrt,"
+      "     tan = math.tan, tanh = math.tanh,"
+      "     random = math.random,"   // This points to our math.random override in registerLooseFunctions()
+      "  },"
+      "  os = { clock = os.clock, difftime = os.difftime, time = os.time },"
+      "}"
+      "";
+
+
 bool LuaScriptRunner::prepareEnvironment()              
 {
    if(!L)
@@ -462,6 +503,11 @@ bool LuaScriptRunner::prepareEnvironment()
    }
 
    TNLAssert(lua_gettop(L) == 0 || LuaBase::dumpStack(L), "Stack dirty!");
+
+   // First register our sandbox as the global namespace
+   luaL_dostring(L, sandbox_env);
+   lua_getglobal(L, "sandbox_e");                        // -- environment sandbox_e
+   lua_setfield(L, LUA_REGISTRYINDEX, getScriptId());    // -- <<empty stack>>
 
    // Register all our classes in the global namespace... they will be copied below when we copy the environment
 
