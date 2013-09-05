78284afec312c121dca0c29996f53b7881cd6635
eykamp <chris@eykamp.com>
2013-09-06 01:11:33 +0200

    Create tests for the generic bit array packing/unpacking functions

--- bitfighter_test/main_test.cpp
+++ bitfighter_test/main_test.cpp
@@ -63,6 +63,50 @@ TEST_F(BfTest, StringUtilsTests)
 } 
 
 
+TEST_F(BfTest, IniSettingsPackUnpack)
+{
+   static const S32 count = 5;
+   bool items[count];
+
+   // Set all bits to false
+   IniSettings::clearbits(items, count);
+   ASSERT_FALSE(items[0] || items[1] || items[2] || items[3] || items[4]);
+
+   // Set from proper length string
+   IniSettings::unpack("YYNNN", items, count);
+   ASSERT_TRUE(items[0] && items[1]);
+   ASSERT_FALSE(items[2] || items[3] || items[4]);
+
+   // Set all bits to false
+   IniSettings::clearbits(items, count);
+   ASSERT_FALSE(items[0] || items[1] || items[2] || items[3] || items[4]);
+
+   // Set from short string
+   IniSettings::unpack("YNY", items, count);
+   ASSERT_TRUE(items[0] && items[2]);
+   ASSERT_FALSE(items[1] || items[3] || items[4]);
+
+   // Set from long string
+   IniSettings::clearbits(items, count);
+   IniSettings::unpack("NNYYYYYYYYYY", items, count);
+   ASSERT_TRUE(items[2] && items[3] && items[4]);
+   ASSERT_FALSE(items[0] || items[1]);
+
+   // Set from bogus string
+   IniSettings::clearbits(items, count);
+   IniSettings::unpack("yyYXE", items, count);
+   ASSERT_TRUE(items[2]);
+   ASSERT_FALSE(items[0] || items[1] || items[3] || items[4]);
+
+   // Now we know unpack works, so testing pack should be easy
+   IniSettings::clearbits(items, count);
+   string vals = "YYNNY";
+
+   IniSettings::unpack(vals, items, count);
+   ASSERT_EQ(IniSettings::pack(items, count), vals);
+}
+
+
 TEST_F(BfTest, SettingsTests)
 {
    Settings settings;
