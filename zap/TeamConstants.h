7ee386f90b141a74e7876b218d44e31028764959
eykamp <chris@eykamp.com>
2013-09-08 13:08:19 +0200

    Comments

--- zap/TeamConstants.h
+++ zap/TeamConstants.h
@@ -33,10 +33,12 @@ using namespace TNL;
 namespace Zap
 {
 
+// Special team constants
 static const S32 TEAM_NEUTRAL = -1;
 static const S32 TEAM_HOSTILE = -2;
 static const S32 NO_TEAM = -3;      // Not exposed to lua, not used in level files, only used internally
-static const S32 NONE = -1;
+
+static const S32 NONE = -1;         // Used for a variety of purposes
 
 };
 
