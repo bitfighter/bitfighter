6a0b954a576def4469457ba9936214154244c34f
eykamp <chris@eykamp.com>
2013-10-04 20:13:51 +0200

    How did this not get noticed earlier??

--- zap/GameTypesEnum.h
+++ zap/GameTypesEnum.h
@@ -36,13 +36,13 @@ namespace Zap
 #define GAME_TYPE_TABLE \
    GAME_TYPE_ITEM( BitmatchGame,    "GameType",            "Bitmatch", "Bitmatch"         ) \
    GAME_TYPE_ITEM( CTFGame,         "CTFGameType",         "CTF",      "Capture the Flag" ) \
-   GAME_TYPE_ITEM( HTFGame,         "CoreGameType",        "HTF",      "Hold the Flag"    ) \
-   GAME_TYPE_ITEM( NexusGame,       "HTFGameType",         "Nexus",    "Nexus"            ) \
-   GAME_TYPE_ITEM( RabbitGame,      "NexusGameType",       "Rabbit",   "Rabbit"           ) \
-   GAME_TYPE_ITEM( RetrieveGame,    "RabbitGameType",      "Retrieve", "Retrieve"         ) \
-   GAME_TYPE_ITEM( SoccerGame,      "RetrieveGameType",    "Soccer",   "Soccer"           ) \
-   GAME_TYPE_ITEM( ZoneControlGame, "SoccerGameType",      "ZC",       "Zone Control"     ) \
-   GAME_TYPE_ITEM( CoreGame,        "ZoneControlGameType", "Core",     "Core"             ) \
+   GAME_TYPE_ITEM( HTFGame,         "HTFGameType",         "HTF",      "Hold the Flag"    ) \
+   GAME_TYPE_ITEM( NexusGame,       "NexusGameType",       "Nexus",    "Nexus"            ) \
+   GAME_TYPE_ITEM( RabbitGame,      "RabbitGameType",      "Rabbit",   "Rabbit"           ) \
+   GAME_TYPE_ITEM( RetrieveGame,    "RetrieveGameType",    "Retrieve", "Retrieve"         ) \
+   GAME_TYPE_ITEM( SoccerGame,      "SoccerGameType",      "Soccer",   "Soccer"           ) \
+   GAME_TYPE_ITEM( ZoneControlGame, "ZoneControlGameType", "ZC",       "Zone Control"     ) \
+   GAME_TYPE_ITEM( CoreGame,        "CoreGameType",        "Core",     "Core"             ) \
 
 
    // Define an enum from the first column of GAME_TYPE_TABLE
