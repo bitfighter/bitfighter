7702cacc3b8bf5a3a21a78b3d7a2516da39d0570
raptor <buckyballreaction@gmail.com>
2013-10-05 11:03:53 -0600

    Update OSX SDL2.framework to stable 2.0.0, rev. 70b305e09b4e

--- lib/SDL2.framework/Versions/A/Headers/SDL_platform.h
+++ lib/SDL2.framework/Versions/A/Headers/SDL_platform.h
@@ -66,7 +66,7 @@
 #endif
 #if defined(ANDROID)
 #undef __ANDROID__
-#undef __LINUX__ /*do we need to do this?*/
+#undef __LINUX__ /* do we need to do this? */
 #define __ANDROID__ 1
 #endif
 
@@ -114,7 +114,7 @@
 #undef __SOLARIS__
 #define __SOLARIS__ 1
 #endif
-#if defined(WIN32) || defined(_WIN32)
+#if defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__)
 #undef __WIN32__
 #define __WIN32__   1
 #endif
--- lib/SDL2.framework/Versions/A/Headers/begin_code.h
+++ lib/SDL2.framework/Versions/A/Headers/begin_code.h
@@ -33,15 +33,17 @@
 #endif
 #define _begin_code_h
 
+#ifndef SDL_DEPRECATED
+#  if (__GNUC__ >= 4)  /* technically, this arrived in gcc 3.1, but oh well. */
+#    define SDL_DEPRECATED __attribute__((deprecated))
+#  else
+#    define SDL_DEPRECATED
+#  endif
+#endif
+
 /* Some compilers use a special export keyword */
 #ifndef DECLSPEC
-# if defined(__BEOS__) || defined(__HAIKU__)
-#  if defined(__GNUC__)
-#   define DECLSPEC __declspec(dllexport)
-#  else
-#   define DECLSPEC __declspec(export)
-#  endif
-# elif defined(__WIN32__)
+# if defined(__WIN32__)
 #  ifdef __BORLANDC__
 #   ifdef BUILD_SDL
 #    define DECLSPEC
@@ -54,6 +56,8 @@
 # else
 #  if defined(__GNUC__) && __GNUC__ >= 4
 #   define DECLSPEC __attribute__ ((visibility("default")))
+#  elif defined(__GNUC__) && __GNUC__ >= 2
+#   define DECLSPEC __declspec(dllexport)
 #  else
 #   define DECLSPEC
 #  endif
@@ -95,11 +99,9 @@
 #endif
 #endif /* Compiler needs structure packing set */
 
+#ifndef __inline__
 /* Set up compiler-specific options for inlining functions */
 #ifndef SDL_INLINE_OKAY
-#ifdef __GNUC__
-#define SDL_INLINE_OKAY
-#else
 /* Add any special compiler-specific cases here */
 #if defined(_MSC_VER) || defined(__BORLANDC__) || \
     defined(__DMC__) || defined(__SC__) || \
@@ -108,31 +110,34 @@
 #ifndef __inline__
 #define __inline__  __inline
 #endif
-#define SDL_INLINE_OKAY
+#define SDL_INLINE_OKAY 1
 #else
 #if !defined(__MRC__) && !defined(_SGI_SOURCE)
 #ifndef __inline__
 #define __inline__ inline
 #endif
-#define SDL_INLINE_OKAY
+#define SDL_INLINE_OKAY 1
 #endif /* Not a funky compiler */
 #endif /* Visual C++ */
-#endif /* GNU C */
 #endif /* SDL_INLINE_OKAY */
 
 /* If inlining isn't supported, remove "__inline__", turning static
    inlined functions into static functions (resulting in code bloat
    in all files which include the offending header files)
 */
-#ifndef SDL_INLINE_OKAY
+#if !SDL_INLINE_OKAY || __STRICT_ANSI__
+#ifdef __inline__
+#undef __inline__
+#endif
 #define __inline__
 #endif
+#endif /* __inline__ not defined */
 
 #ifndef SDL_FORCE_INLINE
 #if defined(_MSC_VER)
 #define SDL_FORCE_INLINE __forceinline
 #elif ( (defined(__GNUC__) && (__GNUC__ >= 4)) || defined(__clang__) )
-#define SDL_FORCE_INLINE __attribute__((always_inline)) static inline
+#define SDL_FORCE_INLINE __attribute__((always_inline)) static __inline__
 #else
 #define SDL_FORCE_INLINE static __inline__
 #endif
