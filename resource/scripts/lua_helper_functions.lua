3a386c378b17b9625b0f445f253f621c320062f1
kaen <bkconrad@gmail.com>
2013-11-07 06:28:47 -0800

    change some leftover license headers

--- resource/scripts/lua_helper_functions.lua
+++ resource/scripts/lua_helper_functions.lua
@@ -1,26 +1,7 @@
--------------------------------------------------------------------------------
---
--- Bitfighter - A multiplayer vector graphics space game
--- Based on Zap demo released for Torque Network Library by GarageGames.com
---
--- Copyright (C) 2008-2009 Chris Eykamp
--- Other code copyright as noted
---
--- This program is free software; you can redistribute it and/or modify
--- it under the terms of the GNU General Public License as published by
--- the Free Software Foundation; either version 2 of the License, or
--- (at your option) any later version.
---
--- This program is distributed in the hope that it will be useful (and fun!),
--- but WITHOUT ANY WARRANTY; without even the implied warranty of
--- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
--- GNU General Public License for more details.
---
--- You should have received a copy of the GNU General Public License
--- along with this program; if not, write to the Free Software
--- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
---
--------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Copyright Chris Eykamp
+-- See LICENSE.txt for full copyright information
+--------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
