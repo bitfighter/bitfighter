7f14401c0b458a8dc6e1743460168e297bdb4626
eykamp <chris@eykamp.com>
2013-11-17 14:16:54 +0100

    Formatting

--- zap/UIGame.cpp
+++ zap/UIGame.cpp
@@ -1211,11 +1211,11 @@ bool GameUserInterface::onKeyDown(InputCode inputCode)
       else if(getGame()->isSpawnDelayed())
       {
          // Allow scoreboard and the various chats while idle
-         if(!checkInputCode(InputCodeManager::BINDING_OUTGAMECHAT, inputCode) &&
-            !checkInputCode(InputCodeManager::BINDING_GLOBCHAT, inputCode)    &&
-            !checkInputCode(InputCodeManager::BINDING_TEAMCHAT, inputCode)    &&
-            !checkInputCode(InputCodeManager::BINDING_CMDCHAT,  inputCode)    &&
-            !checkInputCode(InputCodeManager::BINDING_SCRBRD,   inputCode))
+         if(!checkInputCode(InputCodeManager::BINDING_OUTGAMECHAT, inputCode)  &&
+            !checkInputCode(InputCodeManager::BINDING_GLOBCHAT,    inputCode)  &&
+            !checkInputCode(InputCodeManager::BINDING_TEAMCHAT,    inputCode)  &&
+            !checkInputCode(InputCodeManager::BINDING_CMDCHAT,     inputCode)  &&
+            !checkInputCode(InputCodeManager::BINDING_SCRBRD,      inputCode))
          {
             getGame()->undelaySpawn();
             if(inputCode != KEY_ESCAPE)  // Don't swollow escape: Lagged out and can't un-idle to bring up the menu?
