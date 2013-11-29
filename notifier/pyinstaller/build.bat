2aac464f583ae4a55a60ea508ee8279c9c4a20c3
eykamp <chris@eykamp.com>
2013-11-30 00:48:17 +0100

    Create a bat file to help build the notifier on windows

---
+++ notifier/pyinstaller/build.bat
@@ -0,0 +1,6 @@
+@echo off
+echo Building notifier executable...
+pyinstaller --onefile --noconsole --icon=..\redship48.ico ..\bitfighter_notifier.py
+echo Created .\dist\bitfighter_notifier.exe
+echo Distribute this exe along with the redship48.ico icon file in the parent folder
+echo NSIS will find the exe in the dist folder.
