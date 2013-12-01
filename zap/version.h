a12ea573b0fed0dd7ed4ac713269c42601325877
raptor <buckyballreaction@gmail.com>
2013-11-30 18:41:47 -0700

    Update build number for 019 and update CS_PROTOCOL_VERSION to invalidate all the many beta versions

--- zap/version.h
+++ zap/version.h
@@ -11,11 +11,12 @@
 // Updated from 6 to 7 for 019
 #define MASTER_PROTOCOL_VERSION 7  // Change this when releasing an incompatible cm/sm protocol (must be int)
                                    // MASTER_PROTOCOL_VERSION = 4, client 015a and older (CS_PROTOCOL_VERSION <= 32) can not connect to our new master.
-#define CS_PROTOCOL_VERSION 37     // Change this when releasing an incompatible cs protocol (must be int)
+#define CS_PROTOCOL_VERSION 38     // Change this when releasing an incompatible cs protocol (must be int)
 // 016 = 33 
 // 017[ab] = 35
 // 018[a] = 36
-// 019 = 37
+// 019 dev = 37
+// 019 = 38
 
 #define VERSION_016  3737
 #define VERSION_017  4252
@@ -23,7 +24,7 @@
 #define VERSION_017b 4537
 #define VERSION_018  6059
 #define VERSION_018a 6800
-#define VERSION_019  6819          // TODO: change when releasing 019
+#define VERSION_019  8814
 
 #define BUILD_VERSION VERSION_019 // Version of the game according to hg, will be unique every release (must be int)
                                    // Get from "hg summary"
