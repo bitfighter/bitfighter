ef65a829743eead11e61f4e1fa0a62aa4b64d609
raptor <buckyballreaction@gmail.com>
2013-12-01 15:09:03 -0700

    Fix OSX editor help column rendering.  We should probably have a coding convention of not using global statics in global statics to avoid weird initialization problems like this

--- zap/TimeLeftRenderer.h
+++ zap/TimeLeftRenderer.h
@@ -23,6 +23,8 @@ namespace UI
 class TimeLeftRenderer
 {
 private:
+   U32 mRightAlignCoord;
+
    Point renderTimeLeft      (const GameType *gameType, bool render = true) const;     // Returns width and height
    S32 renderHeadlineScores  (const Game *game, S32 ypos) const;
    S32 renderTeamScores      (const GameType *gameType, S32 bottom, bool render) const;
@@ -30,6 +32,9 @@ private:
 
 public:
    static const S32 TimeLeftIndicatorMargin = 7;
+
+   TimeLeftRenderer();
+   virtual ~TimeLeftRenderer();
 
    Point render(const GameType *gameType, bool scoreboardVisible, bool render) const;
 };
