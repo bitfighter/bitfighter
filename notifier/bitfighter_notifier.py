b1179ef6da425f4a85b16d6cdd840c775af34532
raptor <buckyballreaction@gmail.com>
2013-12-06 21:52:36 -0700

    Only allow one instance of the notifier to run at a time

--- notifier/bitfighter_notifier.py
+++ notifier/bitfighter_notifier.py
@@ -67,6 +67,7 @@ import os
 import subprocess
 import sys
 import threading
+import tempfile   # For our singleton class
 
   
 """
@@ -109,6 +110,70 @@ if devMode == True:
 Logging
 """
 logging.basicConfig(level=loggingLevel)
+
+
+"""
+Singleton class
+
+This prevents multiple notifiers from running.  Taken from:
+   https://github.com/pycontribs/tendo/blob/master/tendo/singleton.py
+"""
+class SingleInstance:
+    def __init__(self, flavor_id=""):
+        self.initialized = False
+        basename = os.path.splitext(os.path.abspath(sys.argv[0]))[0].replace("/", "-").replace(":", "").replace("\\", "-") + '-%s' % flavor_id + '.lock'
+        # os.path.splitext(os.path.abspath(sys.modules['__main__'].__file__))[0].replace("/", "-").replace(":", "").replace("\\", "-") + '-%s' % flavor_id + '.lock'
+        self.lockfile = os.path.normpath(tempfile.gettempdir() + '/' + basename)
+
+        logging.debug("SingleInstance lockfile: " + self.lockfile)
+        if sys.platform == 'win32':
+            try:
+                # file already exists, we try to remove (in case previous execution was interrupted)
+                if os.path.exists(self.lockfile):
+                    os.unlink(self.lockfile)
+                self.fd = os.open(self.lockfile, os.O_CREAT | os.O_EXCL | os.O_RDWR)
+            except OSError:
+                type, e, tb = sys.exc_info()
+                if e.errno == 13:
+                    logging.error("Another instance is already running, quitting.")
+                    sys.exit(-1)
+                print(e.errno)
+                raise
+        else:  # non Windows
+            import fcntl
+            self.fp = open(self.lockfile, 'w')
+            try:
+                fcntl.lockf(self.fp, fcntl.LOCK_EX | fcntl.LOCK_NB)
+            except IOError:
+                logging.warning("Another instance is already running, quitting.")
+                sys.exit(-1)
+        self.initialized = True
+
+    def __del__(self):
+        import sys
+        import os
+        if not self.initialized:
+            return
+        try:
+            if sys.platform == 'win32':
+                if hasattr(self, 'fd'):
+                    os.close(self.fd)
+                    os.unlink(self.lockfile)
+            else:
+                import fcntl
+                fcntl.lockf(self.fp, fcntl.LOCK_UN)
+                # os.close(self.fp)
+                if os.path.isfile(self.lockfile):
+                    os.unlink(self.lockfile)
+        except Exception as e:
+            if logging:
+                logging.warning(e)
+            else:
+                print("Unloggable error: %s" % e)
+            sys.exit(-1)
+
+# Use our singleton
+me = SingleInstance()
 
 
 """
