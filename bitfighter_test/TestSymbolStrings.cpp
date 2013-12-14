8b6c458aba25637db82b2a4664fa172542b00c3d
eykamp <chris@eykamp.com>
2013-12-14 23:21:01 +0100

    Fix test -- all tests passing

--- bitfighter_test/TestSymbolStrings.cpp
+++ bitfighter_test/TestSymbolStrings.cpp
@@ -21,12 +21,14 @@ TEST(SymbolStringTest, Height)
    EXPECT_EQ(10, one->getHeight());
 
    string emptyMsg = "";
+
+   // Empty string with blockMode == true gets height of fontSize
    SymbolShapePtr two = SymbolShapePtr(new SymbolString(emptyMsg, NULL, context, fontSize, true));
-   EXPECT_EQ(10, one->getHeight());
+   EXPECT_EQ(10, two->getHeight());
 
+   // Empty string with blockMode == false gets height of 0
    SymbolShapePtr three = SymbolShapePtr(new SymbolString(emptyMsg, NULL, context, fontSize, false));
-   EXPECT_EQ(0, one->getHeight());
-
+   EXPECT_EQ(0, three->getHeight());
 }
 
    
