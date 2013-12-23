f4b89cfa02396ad2f0defe513845928e77b2cd82
eykamp <chris@eykamp.com>
2013-12-23 18:26:15 +0100

    Remove test

--- bitfighter_test/TestGameType.cpp
+++ bitfighter_test/TestGameType.cpp
@@ -9,16 +9,6 @@
 namespace Zap
 {
 
-TEST(GameTypeTest, GetMaxPlayersPerBalancedTeam)
-{
-   GameType gt;
-
-   // Check the maxPlayersPerTeam fn
-   //                                players--v  v--teams
-   ASSERT_EQ(gt.getMaxPlayersPerBalancedTeam( 1, 1), 1);
-   ASSERT_EQ(gt.getMaxPlayersPerBalancedTeam( 1, 2), 1);
-   ASSERT_EQ(gt.getMaxPlayersPerBalancedTeam(10, 5), 2);
-   ASSERT_EQ(gt.getMaxPlayersPerBalancedTeam(11, 5), 3);
-}
+// Need some tests here!
 
 };
