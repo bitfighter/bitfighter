2f8a7d2f238ff4adc0c35a4e836b82f9263dd6fc
eykamp <chris@eykamp.com>
2014-01-12 02:10:56 +0100

    Typo

--- master/GameJoltConnector.cpp
+++ master/GameJoltConnector.cpp
@@ -72,7 +72,7 @@ static void updateGameJolt(const MasterSettings *settings, const string &baseUrl
 
    if(urlList.length() > 0)
    {
-      logprintf("Curling: %s", urList.c_str());
+      logprintf("Curling: %s", urlList.c_str());
 
       // This is a fallback because the request.send() was returning a "Socket not writable" error
       execl("/usr/bin/curl", "curl", urlList.c_str(), NULL);
