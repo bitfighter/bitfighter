2f90b3dc46ff2e1091f9368ca770e5aef22526b0
raptor <buckyballreaction@gmail.com>
2014-02-03 23:00:50 -0700

    Adjust rendering of slipzone icon slightly

--- zap/gameObjectRender.cpp
+++ zap/gameObjectRender.cpp
@@ -1847,13 +1847,13 @@ void renderSlipZoneIcon(const Point &center, S32 radius, F32 angleRadians)
 {
    static const F32 lines[] = {
          // Lines
-         -15, 8, -3, 8,
-         -20, 0, -10, 0,
-         -16, -8, -4, -8,
+         -16, 7, -6, 7,
+         -20, 2, -10, 2,
+         -16, -3, -6, -3,
          // Ship
-         9.77, 12.37, 16.84, -8.84,
-         16.84, -8.84, -4.37, -1.77,
-         -4.37, -1.77, 9.77, 12.37
+         15.35, 9.61, 10.86, -12.29,
+         10.86, -12.29, -3.97, 4.44,
+         -3.97, 4.44, 15.35, 9.61,
    };
 
 
@@ -1862,7 +1862,6 @@ void renderSlipZoneIcon(const Point &center, S32 radius, F32 angleRadians)
       glScale(radius * 0.05f);  // 1/20.  Default radius is 20 in-game
 
       glColor(Colors::cyan);
-//      renderCenteredString(Point(0,0), 25, "CAUTION - SLIPPERY!");
       renderVertexArray(lines, ARRAYSIZE(lines) / 2, GL_LINES);
    glPopMatrix();
 }
