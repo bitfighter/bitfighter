36d10d3c317a8505d9c7251c71aaddb81a7c7867
raptor <buckyballreaction@gmail.com>
2014-02-25 10:16:05 -0700

    Statically compile LuaJIT library for windows

--- lua/luajit/src/msvcbuild.bat
+++ lua/luajit/src/msvcbuild.bat
@@ -70,6 +70,7 @@ buildvm -m folddef -o lj_folddef.h lj_opt_fold.c
 :NODEBUG
 @if "%1"=="amalg" goto :AMALGDLL
 @if "%1"=="static" goto :STATIC
+@if "%1"=="staticamalg" goto :STATICAMALG
 %LJCOMPILE% /MT /DLUA_BUILD_AS_DLL lj_*.c lib_*.c
 @if errorlevel 1 goto :BAD
 %LJLINK% /DLL /out:%LJDLLNAME% lj_*.obj lib_*.obj
@@ -86,6 +87,13 @@ buildvm -m folddef -o lj_folddef.h lj_opt_fold.c
 @if errorlevel 1 goto :BAD
 %LJLINK% /DLL /out:%LJDLLNAME% ljamalg.obj lj_vm.obj
 @if errorlevel 1 goto :BAD
+@goto :MTDLL
+:STATICAMALG
+%LJCOMPILE% ljamalg.c
+@if errorlevel 1 goto :BAD
+%LJLIB% /OUT:%LJLIBNAME% ljamalg.obj lj_vm.obj
+@if errorlevel 1 goto :BAD
+@goto :MTDLL
 :MTDLL
 if exist %LJDLLNAME%.manifest^
   %LJMT% -manifest %LJDLLNAME%.manifest -outputresource:%LJDLLNAME%;2
