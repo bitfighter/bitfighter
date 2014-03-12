98c795d752bc1d48bf542fd37898aa4c0fccd224
kaen <bkconrad@gmail.com>
2014-03-11 17:12:13 -0700

    add mingw64 toolchain file

---
+++ cmake/Toolchains/Toolchain-mingw64.cmake
@@ -0,0 +1,20 @@
+# the name of the target operating system
+SET(CMAKE_SYSTEM_NAME Windows)
+SET(WIN32 True)
+SET(MINGW True)
+SET(XCOMPILE True)
+
+# which compilers to use for C and C++
+SET(CMAKE_C_COMPILER i686-w64-mingw32-gcc)
+SET(CMAKE_CXX_COMPILER i686-w64-mingw32-g++)
+SET(CMAKE_RC_COMPILER i686-w64-mingw32-windres)
+
+# here is the target environment located
+SET(CMAKE_FIND_ROOT_PATH ${CMAKE_SOURCE_DIR})
+
+# adjust the default behaviour of the FIND_XXX() commands:
+# search headers and libraries in the target environment, search 
+# programs in the host environment
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
