40abe98d8a7e55c9af3b62fca6c98030771e2db6
sam8641 <sam8641@gmail.com>
2014-04-05 01:01:01 -0500

    Prevent broken clients from seeing "Download Recorded Games", Add GameRecordingDownload config option.

--- zap/gameConnection.cpp
+++ zap/gameConnection.cpp
@@ -34,7 +34,7 @@ namespace Zap
 
 TNL_IMPLEMENT_NETCONNECTION(GameConnection, NetClassGroupGame, true);
 
-const U8 GameConnection::CONNECT_VERSION = 0;  // GameConnection's version, for possible future use with changes on compatible versions
+const U8 GameConnection::CONNECT_VERSION = 1;  // GameConnection's version, for possible future use with changes on compatible versions
 
 // Constructor -- used on Server by TNL, not called directly, used when a new client connects to the server
 GameConnection::GameConnection()
@@ -2214,7 +2214,7 @@ void GameConnection::onConnectionEstablished_server()
    mSendableFlags = 0;
    if(mSettings->getIniSettings()->mSettings.getVal<YesNo>(IniKey::AllowMapUpload))
       mSendableFlags |= ServerFlagAllowUpload;
-   if(settings->getIniSettings()->mSettings.getVal<YesNo>(IniKey::GameRecording))
+   if(settings->getIniSettings()->mSettings.getVal<YesNo>(IniKey::GameRecordingDownload) && mConnectionVersion >= 1)  // 019c and earliear have broken file saving
       mSendableFlags |= ServerFlagHasRecordedGameplayDownloads;
 
    s2rSendableFlags(mSendableFlags);
