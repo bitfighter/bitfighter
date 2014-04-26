d35ad48bfa11e8ca27a6d718c1c9188eab8748d2
eykamp <chris@eykamp.com>
2014-04-26 13:33:43 +0200

    Fix comment

--- zap/ServerGame.cpp
+++ zap/ServerGame.cpp
@@ -1094,7 +1094,8 @@ Vector<Vector<S32> > ServerGame::getCategorizedPlayerCountsByTeam() const
 }
 
 
-// This will NOT delete clientInfo... caller is responsible for cleanup!
+// ClientInfos will be stored as RefPtrs, so they will be deleted when all refs are removed.  In other words,
+// ServerGame will manage cleanup.
 void ServerGame::addClient(ClientInfo *clientInfo)
 {
    TNLAssert(!clientInfo->isRobot(), "This only gets called for players");
