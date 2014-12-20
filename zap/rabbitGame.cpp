741db164570968fb19824999f3c0febb28e9597b
raptor <buckyballreaction@gmail.com>
2014-12-20 00:04:47 -0700

    Raging Rabid Rabbit badge slightly harder to achieve - requires 9 kills in a row

--- zap/rabbitGame.cpp
+++ zap/rabbitGame.cpp
@@ -397,7 +397,7 @@ void RabbitGameType::onFlaggerKill(Ship *rabbitShip)
 
    // See if we've acheived our raging rabid rabbit badge
    if(clientInfo->isAuthenticated() &&                      // Player must be authenticated
-      clientInfo->getKillStreak() >= 7 &&                   // Player must have a kill streak of 7 or more
+      clientInfo->getKillStreak() >= 9 &&                   // Player must have a kill streak of 9 or more
       !clientInfo->hasBadge(BADGE_RAGING_RABID_RABBIT) &&   // Player doesn't already have the badge
       getGame()->getPlayerCount() >= 4 &&                   // Game must have 4+ human players
       getGame()->getAuthenticatedPlayerCount() >= 2)        // Two of whom must be authenticated
