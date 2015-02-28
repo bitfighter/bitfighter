6908450b70f637856fcfdc7c5ac4b4781dd310a5
eykamp <chris@eykamp.com>
2015-02-27 20:41:53 -0800

    Add missing test to cmake

--- zap/bitfighter_test.cmake
+++ zap/bitfighter_test.cmake
@@ -25,6 +25,7 @@ set(TEST_SOURCES
 	${CMAKE_SOURCE_DIR}/bitfighter_test/TestMaster.cpp
 	${CMAKE_SOURCE_DIR}/bitfighter_test/TestMove.cpp
 	${CMAKE_SOURCE_DIR}/bitfighter_test/TestObjects.cpp
+	${CMAKE_SOURCE_DIR}/bitfighter_test/TestObjectScope.cpp
 	${CMAKE_SOURCE_DIR}/bitfighter_test/TestPolylineGeometry.cpp
 	${CMAKE_SOURCE_DIR}/bitfighter_test/TestRenderUtils.cpp
 	${CMAKE_SOURCE_DIR}/bitfighter_test/TestRobot.cpp
