3c96df2528085da14bd4a4b21ea09eb626f8e12d
Chris Eykamp <chris@eykamp.com>
2015-10-29 00:44:28 -0700

    Fix tests

--- bitfighter_test/TestColor.cpp
+++ bitfighter_test/TestColor.cpp
@@ -25,11 +25,11 @@ TEST(ColorTest, IniParsing)
 
    // Quasi invalid inputs
    // Comments below indicate how colors currently work, not necessarily how they _should_ work
-   EXPECT_EQ(Colors::yellow, Color::iniValToColor("2 1 0"));      // Numbers higher than 1 are clamped to 1!
-   EXPECT_EQ(Colors::white, Color::iniValToColor("#ff00ff0"));    // Hex colors must have exactly 6 digits... or white!
-   EXPECT_EQ(Colors::white, Color::iniValToColor("#ff00f"));      // Same... white!
-   EXPECT_EQ(Colors::white, Color::iniValToColor("0 1"));         // Two components... no good!
-   EXPECT_EQ(Colors::green, Color::iniValToColor("0 1 0 1"));     // Extra parameters are dropped!
+   EXPECT_TRUE(Colors::yellow  == Color::iniValToColor("2 1 0"));       // Numbers higher than 1 are clamped to 1!
+   EXPECT_TRUE(Colors::magenta == Color::iniValToColor("#ff00ff0"));    // Long strings get truncated
+   EXPECT_TRUE(Color("ff00f0") == Color::iniValToColor("#ff00f"));      // Short strings get padded with trailing 0s
+   EXPECT_TRUE(Colors::white   == Color::iniValToColor("0 1"));         // Two components... no good!
+   EXPECT_TRUE(Colors::green   == Color::iniValToColor("0 1 0 1"));     // Extra parameters are dropped!
 
 
 }
