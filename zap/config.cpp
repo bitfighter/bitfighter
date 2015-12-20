ffb90ad8e8fa7afce76403c9efc1a837e100ae02
Chris Eykamp <chris@eykamp.com>
2015-12-20 01:07:40 -0800

    Remove old comments

--- zap/config.cpp
+++ zap/config.cpp
@@ -507,12 +507,6 @@ static void setDefaultKeyBindings(CIniFile *ini, InputCodeManager *inputCodeMana
     BINDING_TABLE
 #undef BINDING
 
-   // Keys where savedInIni is false are not user-defineable at the moment, mostly because we want consistency
-   // throughout the game, and that would require some real constraints on what keys users could choose.
-   // keyHELP = KEY_F1;
-   // keyLOBBYCHAT = KEY_F5;
-   // keyFPS = KEY_F6;
-   // keyDIAG = KEY_F7;
 }
 
 
