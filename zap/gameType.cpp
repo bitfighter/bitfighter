72ed17bba4174612118629f89c41bee33fa53d66
Chris Eykamp <chris@eykamp.com>
2016-01-22 01:48:56 -0800

    Remove include

--- zap/gameType.cpp
+++ zap/gameType.cpp
@@ -8,7 +8,6 @@
 #include "BanList.h"
 #include "barrier.h"
 #include "game.h"
-#include "GameRecorder.h"
 #include "IniFile.h"          // For CIniFile
 #include "Level.h"
 #include "LineEditorFilterEnum.h"
