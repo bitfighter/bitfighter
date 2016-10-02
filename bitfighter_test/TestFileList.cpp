a723e23d10bb920963c45ca94be0cba4813d1102
Chris Eykamp <chris@eykamp.com>
2016-10-02 07:35:44 -0700

    Remove include

--- bitfighter_test/TestFileList.cpp
+++ bitfighter_test/TestFileList.cpp
@@ -6,7 +6,6 @@
 #include "FileList.h"
 
 #include "gtest/gtest.h"
-#include <string>
 
 namespace Zap
 {
@@ -16,6 +15,5 @@ namespace Zap
       FileList fileList;
       ASSERT_FALSE(fileList.isOk()) << "Needs to be initialized with valid files first!";
    }
-
 
 }
