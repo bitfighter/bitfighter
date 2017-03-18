b4e186934a1497ca42b5533fdba9564b6452bd2d
Chris Eykamp <chris@eykamp.com>
2017-03-18 13:47:51 -0700

    Fix compilation of tests
    Test framework is not runnable at the moment, for some reason.  TBD.

--- bitfighter_test/TestSymbolStrings.cpp
+++ bitfighter_test/TestSymbolStrings.cpp
@@ -17,17 +17,17 @@ TEST(SymbolStringTest, Height)
    S32 fontSize = 10;
    FontContext context = ErrorMsgContext;
    string msg = "Short Test Line";
-   SymbolShapePtr one = SymbolShapePtr(new SymbolString(msg, NULL, context, fontSize, true));
+   SymbolShapePtr one = SymbolShapePtr(new SymbolString(msg, NULL, context, fontSize, Colors::white, true));
    EXPECT_EQ(10, one->getHeight());
 
    string emptyMsg = "";
 
    // Empty string with blockMode == true gets height of fontSize
-   SymbolShapePtr two = SymbolShapePtr(new SymbolString(emptyMsg, NULL, context, fontSize, true));
+   SymbolShapePtr two = SymbolShapePtr(new SymbolString(emptyMsg, NULL, context, fontSize, Colors::white, true));
    EXPECT_EQ(10, two->getHeight());
 
    // Empty string with blockMode == false gets height of 0
-   SymbolShapePtr three = SymbolShapePtr(new SymbolString(emptyMsg, NULL, context, fontSize, false));
+   SymbolShapePtr three = SymbolShapePtr(new SymbolString(emptyMsg, NULL, context, fontSize, Colors::white, false));
    EXPECT_EQ(0, three->getHeight());
 }
 
