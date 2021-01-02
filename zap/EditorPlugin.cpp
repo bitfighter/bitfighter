37cd11282e4eee1b93a6e6fe0f66951fd3826496
Unknown <chris@eykamp.com>
2021-01-01 23:32:02 -0800

    Luadoc: fix warnings

--- zap/EditorPlugin.cpp
+++ zap/EditorPlugin.cpp
@@ -281,6 +281,8 @@ S32 EditorPlugin::lua_getGridSize(lua_State *L)
 }
 
 
+// Note that %t tokens below are just a quoting of t; the % will not be rendered in the docs
+
 /**
  * @luafunc table EditorPlugin::getSelectedObjects()
  * 
@@ -301,7 +303,7 @@ S32 EditorPlugin::lua_getGridSize(lua_State *L)
  * @endcode
  *
  * This result is sorted by the time at which the objects was selected,
- * so 't[1]' will always be the first selected object and 't[#t]' will
+ * so '%t[1]' will always be the first selected object and '%t[#%t]' will
  * always be the last.
  * 
  * @return Table containing all the objects that are currently selected in
