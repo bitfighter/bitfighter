eb5f5aa401df3fe9ab8ee0c67a225fd7881db9a9
raptor <buckyballreaction@gmail.com>
2021-02-23 21:02:22 -0700

    Checkpoint in predicting SpeedZone end points for botzones. Works well in most cases, including bouncing off barriers. Some clean-up and corner cases still to do.

--- zap/BotNavMeshZone.cpp
+++ zap/BotNavMeshZone.cpp
@@ -360,7 +360,7 @@ void BotNavMeshZone::populateZoneList(GridDatabase *botZoneDatabase, Vector<BotN
 
 // Only runs on server
 static void linkConnectionsTeleporters(const GridDatabase *botZoneDatabase,
-                                                     const Vector<pair<Point, const Vector<Point> *> > &teleporterData)
+      const Vector<pair<Point, const Vector<Point> *> > &teleporterData)
 {
    NeighboringZone neighbor;
    // Now create paths representing the teleporters
@@ -396,6 +396,253 @@ static void linkConnectionsTeleporters(const GridDatabase *botZoneDatabase,
          }
       }  // for loop iterating over teleporter dests
    } // for loop iterating over teleporters
+}
+
+
+bool isSpeedZoneProblemObject(U8 x)
+{
+   return
+         // BarrierType is the server-side degenerate object for BarrierMaker and PolyWall
+         x == BarrierTypeNumber ||
+         x == SpeedZoneTypeNumber || x == TeleporterTypeNumber;
+}
+
+
+//static BfObject *findWallCollision(const Point &vel, F32 &collisionTime, Point &collisionPoint)
+//{
+//   // Check for collisions against other objects
+//   Point delta = getVel(stateIndex) * collisionTime;
+//
+//   Rect queryRect(getPos(stateIndex), getPos(stateIndex) + delta);
+//   queryRect.expand(Point(Ship::CollisionRadius, Ship::CollisionRadius));
+//
+//   fillVector.clear();
+//
+//   findObjects(collideTypes(), fillVector, queryRect);   // Free CPU for finding only the ones we care about
+//
+//   fillVector.sort(sortBarriersFirst);  // Sort to do Barriers::Collide first, to prevent picking up flag (FlagItem::Collide) through Barriers, especially when client does /maxfps 10
+//
+//   F32 collisionFraction;
+//
+//   BfObject *collisionObject = NULL;
+//
+//   for(S32 i = 0; i < fillVector.size(); i++)
+//   {
+//      BfObject *foundObject = static_cast<BfObject *>(fillVector[i]);
+//
+//      if(!foundObject->isCollisionEnabled())
+//         continue;
+//
+//      const Vector<Point> *poly = foundObject->getCollisionPoly();
+//
+//      if(poly)
+//      {
+//         Point cp;
+//
+//         if(PolygonSweptCircleIntersect(&poly->first(), poly->size(), getPos(stateIndex),
+//                                        delta, mRadius, cp, collisionFraction))
+//         {
+//            if(cp != getPos(stateIndex) || !isCollideableType(foundObject->getObjectTypeNumber()))   // Avoid getting stuck inside polygon wall
+//            {
+//               bool collide1 = collide(foundObject);
+//               bool collide2 = foundObject->collide(this);
+//
+//               if(!(collide1 && collide2))
+//                  continue;
+//
+//               collisionPoint = cp;
+//               delta *= collisionFraction;
+//               collisionTime *= collisionFraction;
+//               collisionObject = foundObject;
+//
+//               if(!collisionTime)
+//                  break;
+//            }
+//         }
+//      }
+//   }
+//
+//   return collisionObject;
+//}
+
+
+static void linkConnectionsSpeedZones(const GridDatabase *gameObjDatabase,
+      const GridDatabase *botZoneDatabase, Vector<BotNavMeshZone *> *allZones,
+      const Vector<DatabaseObject *> &speedZoneList, const Vector<Vector<Point> > &speedZonePolygons,
+      S32 szBotZoneStartId)
+{
+   NeighboringZone neighbor;
+   Point origin;       // SpeedZone origin point
+   Point source, dest; // Source and destination points for the current travelling vector
+
+   // Go through each known SpeedZone
+   for(S32 i = 0; i < speedZoneList.size(); i++)
+   {
+      logprintf("newzone");
+      SpeedZone *speedZone = static_cast<SpeedZone *>(speedZoneList[i]);
+      const Vector<Point> &szBufferedPoly = speedZonePolygons[i];  // Aligned with speedZoneList
+
+      Point dir = speedZone->getVert(1) - speedZone->getVert(0);  // end - start
+
+      // Determine the likely end point based on the initial speed of the SpeedZone
+      // It has a 1.5 multiplier (see SpeedZone::collided()) for some reason
+      U16 currentSpeed = speedZone->getSpeed() * SpeedZone::SpeedMultiplier;
+
+      // Physics: v_i^2 / 2a
+      F32 distanceEstimate = sq(currentSpeed) / (2 * Ship::Acceleration);
+      Point szTip = speedZone->getOutline()->get(2);  // Tip point 2, from SpeedZone::generatePoints()
+
+      if(distanceEstimate < Ship::CollisionRadius)  // Clamp to lower bound
+         distanceEstimate = Ship::CollisionRadius;
+
+
+      Point currentVec = dir;
+      currentVec.normalize(distanceEstimate);   // Vector from the SZ
+
+      source = szTip;
+      dest = szTip + currentVec;  // Will likely be overridden
+
+
+      DatabaseObject* hitObject = NULL;
+      F32 currentVecDist = distanceEstimate;
+      // Used for bouncing off Barriers
+      F32 collisionTime;
+      Point collisionNormal;
+
+      // Here we'll attempt to see if the speed zone runs us into another object,
+      // even after bouncing off of walls multiple times
+      bool objSearch = true;
+
+      while(objSearch)
+      {
+         // TODO Use findFirstCollision
+         hitObject = gameObjDatabase->findObjectLOS((TestFunc) isSpeedZoneProblemObject,
+               ActualState, source, dest, collisionTime, collisionNormal);
+
+         logprintf("source: %f,%f; dest: %f,%f; norm: %f,%f",
+               source.x, source.y, dest.x, dest.y, collisionNormal.x, collisionNormal.y);
+
+         if(hitObject)  // Some object in the way
+         {
+            U8 hitObjectType = hitObject->getObjectTypeNumber();
+//            logprintf("hit objtype: %d", hitObjectType);
+
+            if(hitObjectType == SpeedZoneTypeNumber)
+            {
+               // If speedzone, set end point to new speedzone
+               SpeedZone* hitSpeedZone = static_cast<SpeedZone*>(hitObject);
+               // Center of outline
+               dest = (hitSpeedZone->getOutline()->get(2) + hitSpeedZone->getPos()) * 0.5;
+
+               objSearch = false;
+            }
+            else if(hitObjectType == TeleporterTypeNumber)
+            {
+               // If teleporter, set end point to it's position
+               Teleporter* teleporter = static_cast<Teleporter*>(hitObject);
+               dest = teleporter->getPos();
+
+               objSearch = false;
+            }
+            else if(hitObjectType == BarrierTypeNumber)  // Includes both BarrierMaker/Polywall
+            {
+//               logprintf("ct: %f; normal: %f,%f", collisionTime, collisionNormal.x, collisionNormal.y);
+               F32 distBeforeWall = collisionTime * currentVecDist;
+
+               // If we're close enough set the end point near the wall
+               if(currentVecDist - distBeforeWall < 150)  // Somewhat arbitrary
+               {
+                  F32 newDist = distBeforeWall - 2 * Ship::CollisionRadius;  // Dont get stuck off-botzone
+                  currentVec.normalize(newDist);
+
+                  // Re set destination
+                  dest = source + currentVec;
+
+                  objSearch = false;
+               }
+               // Much shorter, include a bounce
+               else
+               {
+                  // calc new v from d, apply elasticity, calc new d
+                  F32 collisionSpeed = sqrt(2 * -Ship::Acceleration * distBeforeWall + sq(currentSpeed));
+                  Point collisionVel = currentVec;
+                  collisionVel.normalize(collisionSpeed);
+                  // Taken from MoveObject::computeCollisionResponseBarrier()
+                  Point reflectVel = collisionVel -
+                        collisionNormal * MoveObject::CollisionElasticity * collisionNormal.dot(collisionVel);
+                  F32 reflectSpeed = reflectVel.len();
+                  F32 remainingDist = sq(reflectSpeed) / (2 * Ship::Acceleration);
+
+//                  logprintf("hitspeed: %f, reflectspeed: %f, dist: %f, remain: %f",
+//                        collisionSpeed, reflectSpeed, distBeforeWall, remainingDist);
+
+                  Point newVec = reflectVel;
+                  newVec.normalize(remainingDist);
+
+                  // Distance vector traveled
+                  Point thisVec = currentVec;
+                  thisVec.normalize(distBeforeWall - 0.01);  // Subtract a little to be off-Barrier
+
+                  // Update new reflected source and destination
+                  source = source + thisVec;
+                  dest = source + newVec;
+
+                  // Update new endVec
+                  currentVec = newVec;
+                  currentVecDist = remainingDist;
+                  currentSpeed = reflectSpeed;
+
+                  objSearch = true;
+               }
+            }
+            else  // Some other weird hit object?
+            {
+               // Minimum point the SpeedZone will send you is directly in front of it;
+               // this is our fallback end point
+               Point dirMin = dir;
+               dirMin.normalize(Ship::CollisionRadius);
+               dest = source + dirMin;
+            }
+         }
+
+         // Nothing in the way so use our last updated destination point
+         else
+             objSearch = false;
+      }
+
+      logprintf("final dest: %f,%f", dest.x, dest.y);
+
+      // Now go through all known bot zones under speedzones
+      // allZones should be a list of BotNavMeshZone ordered by their zoneId
+      // We know ahead of time what ID zones below SpeedZone start at
+      for(S32 j = szBotZoneStartId; j < allZones->size(); j++)
+      {
+         BotNavMeshZone *origZone = allZones->get(j);
+         origin = origZone->getCenter();
+
+         // Test if this bot zone center is within the buffered polygon, if so, this
+         // speedzone 'owns' this botzone
+         if(polygonContainsPoint(&szBufferedPoly[0], szBufferedPoly.size(), origin))
+         {
+            BotNavMeshZone *destZone =
+                  findZoneTouchingCircle(botZoneDatabase, dest, 1);  // Small radius needed only
+
+            if(destZone != NULL && origZone != destZone)      // Ignore teleporters that begin and end in the same zone
+            {
+               // SpeedZone is one way path
+               neighbor.zoneID = destZone->getZoneId();
+               neighbor.borderStart.set(origin);
+               neighbor.borderEnd.set(dest);
+               neighbor.borderCenter.set(origin);
+
+               neighbor.distTo = 0;  // Not sure what this should be
+               neighbor.center.set(origin);
+
+               origZone->mNeighbors.push_back(neighbor);
+            }
+         }
+      }
+   }
 }
 
 
@@ -447,29 +694,9 @@ bool BotNavMeshZone::buildBotMeshZones(GridDatabase *botZoneDatabase, GridDataba
       teleporterData.push_back(teldat);
    }
 
-   // So do speedzones // TODO use speed to connect an end point
+   // So do speedzones
    Vector<DatabaseObject *> speedZoneList;
    gameObjDatabase->findObjects(SpeedZoneTypeNumber, speedZoneList);
-
-//   Vector<pair<Point, Point> > speedZoneData(fillVector.size());
-//   pair<Point, Point> speedZoneDat;
-//
-//   for(S32 i = 0; i < fillVector.size(); i++)
-//   {
-//      SpeedZone *speedZone = static_cast<SpeedZone *>(fillVector[i]);
-//
-//      speedZoneDat.first  = speedZone->getPos();
-//
-//      // TODO
-//      // Calculate destination
-//      // Only add it if it's line-of-sight
-////      speedZoneDat.second = speedZone->getDestList();
-//
-//      speedZoneData.push_back(speedZoneDat);
-//   }
-
-   // TODO link speedzone stuff below
-   // make speedzones have one-way links from zones around it
 
    // Cores can be destroyed and open up new paths - need special handling
    Vector<DatabaseObject *> coreList;
@@ -708,7 +935,7 @@ bool BotNavMeshZone::buildBotMeshZones(GridDatabase *botZoneDatabase, GridDataba
    Vector<S32> polyToZoneMap;
    polyToZoneMap.resize(mesh.npolys);
 
-   // Visualize rcPolyMesh
+   // Generate BotNavMeshZone objects for each valid polygon
    for(S32 i = 0; i < mesh.npolys; i++)
    {
       S32 j = 0;
@@ -760,12 +987,19 @@ bool BotNavMeshZone::buildBotMeshZones(GridDatabase *botZoneDatabase, GridDataba
    if(addedZones)
       populateZoneList(botZoneDatabase, allZones);
 
+
+   // Build connections between zones
    // Build connections for standard zones
    buildConnectionsRecastStyle(allZones, mesh, polyToZoneMap,
          coreRecastPolyStartIdx, szRecastPolyStartIdx);
 
    // Teleporters require special connections
    linkConnectionsTeleporters(botZoneDatabase, teleporterData);
+
+   // And SpeedZones
+   S32 szBotZoneStartId = polyToZoneMap[szRecastPolyStartIdx];
+   linkConnectionsSpeedZones(gameObjDatabase, botZoneDatabase, allZones,
+         speedZoneList, speedZonePolygons, szBotZoneStartId);
 
 #ifdef LOG_TIMER
    U32 done3 = Platform::getRealMilliseconds();  // Done
