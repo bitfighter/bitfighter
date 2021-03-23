f0db5a217c2d9bf01a0817b3a66cd66001c549cd
Chris <chris@eykamp.com>
2021-03-22 19:51:46 -0700

    Comment

--- zap/LuaScriptRunner.cpp
+++ zap/LuaScriptRunner.cpp
@@ -398,6 +398,16 @@ bool LuaScriptRunner::runCmd(const char* function, S32 argCount, S32 returnValue
          // in which it should be argCount + 1
          // top = # items on stack - 2 [_stackTracer and function we want to run] - argCount + 1 [top of stack is 1 not 0]
          S32 top = lua_gettop(L) - 2 - argCount + 1;
+
+         // This assert is  intended to check that if we're running onDataReceived, there should be argCount + 1 items on the stack, 
+         // but for any other function, there should be 1.  This is because onDataReceived, unlike other functions, needs to keep some 
+         // items on the stack in order to be able to replicate them for subsequent calls of the function (if, for example, we're 
+         // sending data to multiple bots).  Because the arguments can be unbounded in type and number, we never copy them into
+         // C++ land; we just duplicate them from the stack as needed.  For other functions, we have the arguments in C++, so we 
+         // can just push them onto the stack multiple times for firing an event for multiple listeners.
+         //
+         // It's a bit messy because we're trying to pack those two conditions into a single Assert that will disappear in production
+         // builds.
          TNLAssert(top == 1 * abs(strcmp(function, "onDataReceived")) + (argCount + 1) * (1 - abs(strcmp(function, "onDataReceived"))), \
             "Unexpected number of items on stack!");
          lua_insert(L, top);                                // -- <<whatever>>, function, <<args>>, _stackTracer
