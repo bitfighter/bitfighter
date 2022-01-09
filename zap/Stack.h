f710518a2fcdc76db03db9b3d17aef60bc168532
Carl Hewett <corteocarl@gmail.com>
2022-01-09 00:00:25 -0500

    Added Stack class

---
+++ zap/Stack.h
@@ -0,0 +1,22 @@
+//------------------------------------------------------------------------------
+// Copyright Chris Eykamp
+// See LICENSE.txt for full copyright information
+//------------------------------------------------------------------------------
+
+// A simple, but fast stack structure
+
+#ifndef _STACK_H_
+#define _STACK_H_
+
+namespace Zap
+{
+
+class Stack
+{
+
+public:
+};
+
+}
+
+#endif // _STACK_H_ 
