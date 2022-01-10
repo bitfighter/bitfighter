3e40af2043b4f17655d55a7170d7a676010bae41
Carl Hewett <corteocarl@gmail.com>
2022-01-09 22:06:24 -0500

    Fix Shader indentation

--- zap/Shader.cpp
+++ zap/Shader.cpp
@@ -20,46 +20,46 @@ namespace Zap
 
 std::string getGLShaderDebugLog(U32 object, PFNGLGETSHADERIVPROC glGet_iv, PFNGLGETSHADERINFOLOGPROC glGet_InfoLog)
 {
-	GLint logLength;
-	std::string log;
-	
-	// Resize string first
-	glGet_iv(static_cast<GLuint>(object), GL_INFO_LOG_LENGTH, &logLength);
-	log.resize(logLength);
+   GLint logLength;
+   std::string log;
+   
+   // Resize string first
+   glGet_iv(static_cast<GLuint>(object), GL_INFO_LOG_LENGTH, &logLength);
+   log.resize(logLength);
 
-	if(logLength)
-		glGet_InfoLog(object, logLength, NULL, &log[0]);
+   if(logLength)
+      glGet_InfoLog(object, logLength, NULL, &log[0]);
 
-	// Remove null terminator added by OpenGL; std::string doesn't need this!
-	log.pop_back();
-	return "\n----------- GL DEBUG LOG -----------\n" + log;
+   // Remove null terminator added by OpenGL; std::string doesn't need this!
+   log.pop_back();
+   return "\n----------- GL DEBUG LOG -----------\n" + log;
 }
 
 Shader::Shader(const std::string& name, const std::string& vertexShaderFile, const std::string& fragmentShaderFile)
-	: mName(name)
-	, mId(0)
-	, mUniformLocations()
+   : mName(name)
+   , mId(0)
+   , mUniformLocations()
    , mLastAlpha(0)
    , mLastPointSize(0)
    , mLastTime(0)
    , mLastIsAlphaTexture(false)
    , mLastTextureSampler(0)
 {
-	std::string vertexShaderCode = getShaderSource(vertexShaderFile);
-	std::string fragmentShaderCode = getShaderSource(fragmentShaderFile);
+   std::string vertexShaderCode = getShaderSource(vertexShaderFile);
+   std::string fragmentShaderCode = getShaderSource(fragmentShaderFile);
 
-	U32 vertexShader = compileShader(vertexShaderFile, vertexShaderCode, GL_VERTEX_SHADER);
-	U32 fragmentShader = compileShader(fragmentShaderFile, fragmentShaderCode, GL_FRAGMENT_SHADER);
+   U32 vertexShader = compileShader(vertexShaderFile, vertexShaderCode, GL_VERTEX_SHADER);
+   U32 fragmentShader = compileShader(fragmentShaderFile, fragmentShaderCode, GL_FRAGMENT_SHADER);
 
-	if(vertexShader != 0 && fragmentShader != 0)
-	{
-		mShaders.push_back(vertexShader);
-		mShaders.push_back(fragmentShader);
-		mId = linkShader(mName, static_cast<GLuint>(vertexShader), static_cast<GLuint>(fragmentShader));
-	}
+   if(vertexShader != 0 && fragmentShader != 0)
+   {
+      mShaders.push_back(vertexShader);
+      mShaders.push_back(fragmentShader);
+      mId = linkShader(mName, static_cast<GLuint>(vertexShader), static_cast<GLuint>(fragmentShader));
+   }
 
-	registerUniforms();
-	registerAttributes();
+   registerUniforms();
+   registerAttributes();
 
    // Set initial uniform values
    glUseProgram(mId);
@@ -73,96 +73,96 @@ Shader::Shader(const std::string& name, const std::string& vertexShaderFile, con
 
 Shader::~Shader()
 {
-	for(U32 shader : mShaders)
-		glDeleteShader(static_cast<GLuint>(shader));
+   for(U32 shader : mShaders)
+      glDeleteShader(static_cast<GLuint>(shader));
 
-	glDeleteProgram(static_cast<GLuint>(mId));
+   glDeleteProgram(static_cast<GLuint>(mId));
 }
 
 // Static
 std::string Shader::getShaderSource(const std::string &fileName)
 {
-	std::string filePath = GameSettings::getFolderManager()->findShaderFile(fileName);
-	TNLAssert(filePath != "", "Expected a shader path here!");
+   std::string filePath = GameSettings::getFolderManager()->findShaderFile(fileName);
+   TNLAssert(filePath != "", "Expected a shader path here!");
 
-	return readFile(filePath);
+   return readFile(filePath);
 }
 
 // Static
 U32 Shader::compileShader(const std::string& shaderPath, const std::string& shaderCode, U32 type)
 {
-	GLint shaderLength = static_cast<GLint>(shaderCode.length());
-	GLuint shader = glCreateShader(static_cast<GLuint>(type));
+   GLint shaderLength = static_cast<GLint>(shaderCode.length());
+   GLuint shader = glCreateShader(static_cast<GLuint>(type));
 
-	// Verify shader code
-	TNLAssert(shaderCode.length() < std::numeric_limits<int>::max(),
-		("Overflow! Shader '" + shaderPath + "' too long! How is this possible?!").c_str());
-	TNLAssert(shaderLength > 0, ("Shader '" + shaderPath + "' is empty!").c_str());
+   // Verify shader code
+   TNLAssert(shaderCode.length() < std::numeric_limits<int>::max(),
+      ("Overflow! Shader '" + shaderPath + "' too long! How is this possible?!").c_str());
+   TNLAssert(shaderLength > 0, ("Shader '" + shaderPath + "' is empty!").c_str());
 
-	const char *shaderFiles[] = { shaderCode.c_str() };
-	const GLint shaderFilesLength[] = { shaderLength };
-	
-	// Compile
-	glShaderSource(shader, 1, shaderFiles, shaderFilesLength);
-	glCompileShader(shader);
+   const char *shaderFiles[] = { shaderCode.c_str() };
+   const GLint shaderFilesLength[] = { shaderLength };
+   
+   // Compile
+   glShaderSource(shader, 1, shaderFiles, shaderFilesLength);
+   glCompileShader(shader);
 
-	// Verify compilation
-	GLint shaderOk;
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &shaderOk);
-	if(!shaderOk)
-	{
-		std::string shaderLog = getGLShaderDebugLog(shader, glGetShaderiv, glGetShaderInfoLog);
-		glDeleteShader(shader);
+   // Verify compilation
+   GLint shaderOk;
+   glGetShaderiv(shader, GL_COMPILE_STATUS, &shaderOk);
+   if(!shaderOk)
+   {
+      std::string shaderLog = getGLShaderDebugLog(shader, glGetShaderiv, glGetShaderInfoLog);
+      glDeleteShader(shader);
 
-		logprintf(shaderLog.c_str());
-		TNLAssert(false, ("Failed to compile shader at '" + shaderPath + "'.").c_str());
-	}
+      logprintf(shaderLog.c_str());
+      TNLAssert(false, ("Failed to compile shader at '" + shaderPath + "'.").c_str());
+   }
 
-	return static_cast<U32>(shader);
+   return static_cast<U32>(shader);
 }
 
 // Static
 U32 Shader::linkShader(const std::string& shaderProgramName, U32 vertexShader, U32 fragmentShader)
 {
-	GLint programOk;
+   GLint programOk;
 
-	GLuint program = glCreateProgram();
-	glAttachShader(program, static_cast<GLuint>(vertexShader));
-	glAttachShader(program, static_cast<GLuint>(fragmentShader));
-	glLinkProgram(program);
+   GLuint program = glCreateProgram();
+   glAttachShader(program, static_cast<GLuint>(vertexShader));
+   glAttachShader(program, static_cast<GLuint>(fragmentShader));
+   glLinkProgram(program);
 
-	glGetProgramiv(program, GL_LINK_STATUS, &programOk);
+   glGetProgramiv(program, GL_LINK_STATUS, &programOk);
 
-	if(!programOk)
-	{
-		std::string shaderLog = getGLShaderDebugLog(program, glGetProgramiv, glGetProgramInfoLog);
-		glDeleteProgram(program);
+   if(!programOk)
+   {
+      std::string shaderLog = getGLShaderDebugLog(program, glGetProgramiv, glGetProgramInfoLog);
+      glDeleteProgram(program);
 
-		logprintf(shaderLog.c_str());
-		TNLAssert(false, ("Failed to link shader program '" + shaderProgramName + "'.").c_str());
-		return 0;
-	}
+      logprintf(shaderLog.c_str());
+      TNLAssert(false, ("Failed to link shader program '" + shaderProgramName + "'.").c_str());
+      return 0;
+   }
 
-	return program;
+   return program;
 }
 
 void Shader::registerUniforms()
 {
-	// glGetUniformLocation returns -1 if uniform was not found
-	mUniformLocations[static_cast<unsigned>(UniformName::MVP)] = glGetUniformLocation(mId, "MVP");
-	mUniformLocations[static_cast<unsigned>(UniformName::Color)] = glGetUniformLocation(mId, "color");
+   // glGetUniformLocation returns -1 if uniform was not found
+   mUniformLocations[static_cast<unsigned>(UniformName::MVP)] = glGetUniformLocation(mId, "MVP");
+   mUniformLocations[static_cast<unsigned>(UniformName::Color)] = glGetUniformLocation(mId, "color");
    mUniformLocations[static_cast<unsigned>(UniformName::PointSize)] = glGetUniformLocation(mId, "pointSize");
-	mUniformLocations[static_cast<unsigned>(UniformName::TextureSampler)] = glGetUniformLocation(mId, "textureSampler");
-	mUniformLocations[static_cast<unsigned>(UniformName::IsAlphaTexture)] = glGetUniformLocation(mId, "isAlphaTexture");
-	mUniformLocations[static_cast<unsigned>(UniformName::Time)] = glGetUniformLocation(mId, "time");
+   mUniformLocations[static_cast<unsigned>(UniformName::TextureSampler)] = glGetUniformLocation(mId, "textureSampler");
+   mUniformLocations[static_cast<unsigned>(UniformName::IsAlphaTexture)] = glGetUniformLocation(mId, "isAlphaTexture");
+   mUniformLocations[static_cast<unsigned>(UniformName::Time)] = glGetUniformLocation(mId, "time");
 }
 
 void Shader::registerAttributes()
 {
-	// glGetAttribLocation returns -1 if attribute was not found
-	mAttributeLocations[static_cast<unsigned>(AttributeName::VertexPosition)] = glGetAttribLocation(mId, "vertexPosition_modelspace");
-	mAttributeLocations[static_cast<unsigned>(AttributeName::VertexColor)] = glGetAttribLocation(mId, "vertexColor");
-	mAttributeLocations[static_cast<unsigned>(AttributeName::VertexUV)] = glGetAttribLocation(mId, "vertexUV");
+   // glGetAttribLocation returns -1 if attribute was not found
+   mAttributeLocations[static_cast<unsigned>(AttributeName::VertexPosition)] = glGetAttribLocation(mId, "vertexPosition_modelspace");
+   mAttributeLocations[static_cast<unsigned>(AttributeName::VertexColor)] = glGetAttribLocation(mId, "vertexColor");
+   mAttributeLocations[static_cast<unsigned>(AttributeName::VertexUV)] = glGetAttribLocation(mId, "vertexUV");
 
    // Enable all used attributes
    for(unsigned i = 0; i < static_cast<unsigned>(AttributeName::AttributeName_LAST); ++i)
@@ -174,12 +174,12 @@ void Shader::registerAttributes()
 
 std::string Shader::getName() const
 {
-	return mName;
+   return mName;
 }
 
 U32 Shader::getId() const
 {
-	return mId;
+   return mId;
 }
 
 S32 Shader::getUniformLocation(UniformName uniformName) const
--- zap/Shader.h
+++ zap/Shader.h
@@ -34,32 +34,32 @@ namespace Zap
 
 enum class UniformName
 {
-	MVP = 0,
-	Color,
+   MVP = 0,
+   Color,
    PointSize,
-	TextureSampler,
-	IsAlphaTexture,
-	Time,
-	UniformName_LAST // Keep this at the end
+   TextureSampler,
+   IsAlphaTexture,
+   Time,
+   UniformName_LAST // Keep this at the end
 };
 
 enum class AttributeName
 {
-	VertexPosition = 0,
-	VertexColor,
-	VertexUV,
-	AttributeName_LAST // Keep this at the end
+   VertexPosition = 0,
+   VertexColor,
+   VertexUV,
+   AttributeName_LAST // Keep this at the end
 };
 
 class Shader
 {
 private:
-	std::string mName;
-	U32 mId;
-	std::vector<U32> mShaders;
+   std::string mName;
+   U32 mId;
+   std::vector<U32> mShaders;
 
-	S32 mUniformLocations[static_cast<unsigned>(UniformName::UniformName_LAST)];
-	S32 mAttributeLocations[static_cast<unsigned>(AttributeName::AttributeName_LAST)];
+   S32 mUniformLocations[static_cast<unsigned>(UniformName::UniformName_LAST)];
+   S32 mAttributeLocations[static_cast<unsigned>(AttributeName::AttributeName_LAST)];
 
    Color mLastColor;
    F32 mLastAlpha;
@@ -68,19 +68,19 @@ private:
    bool mLastIsAlphaTexture;
    U32 mLastTextureSampler;
 
-	static std::string getShaderSource(const std::string &fileName);
-	static U32 compileShader(const std::string& shaderPath, const std::string& shaderCode, U32 type);
-	static U32 linkShader(const std::string& shaderProgramName, U32 vertexShader, U32 fragmentShader);
+   static std::string getShaderSource(const std::string &fileName);
+   static U32 compileShader(const std::string& shaderPath, const std::string& shaderCode, U32 type);
+   static U32 linkShader(const std::string& shaderProgramName, U32 vertexShader, U32 fragmentShader);
 
-	void registerUniforms();
-	void registerAttributes();
+   void registerUniforms();
+   void registerAttributes();
 
 public:
-	Shader(const std::string &name, const std::string &vertexShaderFile, const std::string &fragmentShaderFile);
-	~Shader();
+   Shader(const std::string &name, const std::string &vertexShaderFile, const std::string &fragmentShaderFile);
+   ~Shader();
 
-	std::string getName() const;
-	U32 getId() const;
+   std::string getName() const;
+   U32 getId() const;
 
    S32 getUniformLocation(UniformName uniformName) const;
    S32 getAttributeLocation(AttributeName attributeName) const;
