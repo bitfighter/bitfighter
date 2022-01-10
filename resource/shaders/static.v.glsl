99ab675bd6868a25f8bf378bcb998f16d5f43ead
Carl Hewett <corteocarl@gmail.com>
2022-01-09 22:01:12 -0500

    Added gl_PointSize to relevant shaders

--- resource/shaders/static.v.glsl
+++ resource/shaders/static.v.glsl
@@ -9,9 +9,11 @@
 
 attribute vec2 vertexPosition_modelspace; // Bitfighter gives 2D points!
 uniform mat4 MVP;
+uniform float pointSize;
 
 void main()
 {
 	vec4 v = vec4(vertexPosition_modelspace, 0, 1); // 1 since this is a point.
 	gl_Position = vec4((MVP * v).xyz, 1); // Set the W to 1 just in-case
+	gl_PointSize = pointSize;
 }
