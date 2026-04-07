ff6833eb6c73d5ade3f71e9cf746568d26dfa834
eykamp <chris@eykamp.com>
2026-04-06 22:13:34 -0700

    Whitespace

--- zap/Intervals.h
+++ zap/Intervals.h
@@ -13,12 +13,12 @@ using namespace TNL;
 static const S32 ONE_SECOND    = 1000;    // milliseconds
 static const S32 ONE_MINUTE    = 60 * ONE_SECOND;
 static const S32 ONE_HOUR      = 60 * ONE_MINUTE;
-                               
-static const S32 TWO_SECONDS   =  2 * ONE_SECOND; 
+
+static const S32 TWO_SECONDS   =  2 * ONE_SECOND;
 static const S32 THREE_SECONDS =  3 * ONE_SECOND;
 static const S32 FOUR_SECONDS  =  4 * ONE_SECOND;
 static const S32 FIVE_SECONDS  =  5 * ONE_SECOND;
-static const S32 SIX_SECONDS   =  6 * ONE_SECOND; 
+static const S32 SIX_SECONDS   =  6 * ONE_SECOND;
 static const S32 TEN_SECONDS   = 10 * ONE_SECOND;
 
 static const S32 FIFTEEN_SECONDS = 15 * ONE_SECOND;
@@ -26,7 +26,7 @@ static const S32 TWENTY_SECONDS  = 20 * ONE_SECOND;
 static const S32 THIRTY_SECONDS  = 30 * ONE_SECOND;
 
 static const S32 TEN_MINUTES   = 10 * ONE_MINUTE;
-                               
+
 static const S32 TWO_HOURS     =  2 * ONE_HOUR;
 
 // Conversion factors
