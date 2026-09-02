d5051ff51897f5abc22df30b8c0e076caaf6678f
Noah <noah.webber@pmg.com>
2026-09-02 16:01:07 -0500

    Revert tnl/platform.cpp, zap/bitfighter_test.cmake, and CMakeLists.txt to match master

--- tnl/platform.cpp
+++ tnl/platform.cpp
@@ -357,6 +357,18 @@ void Platform::forceQuit()
 static bool sg_initialized = false;
 static timeval sg_startTime;
 
+static void x86UNIXInitTimer()
+{
+   if(!sg_initialized)
+   {
+      sg_initialized = true;
+      ::gettimeofday(&sg_startTime, NULL);
+   }
+}
+
+static bool sg_initialized = false;
+static timeval sg_startTime;
+
 static void x86UNIXTimerInit()
 {
    if (sg_initialized == false) {
@@ -390,16 +402,6 @@ U32 x86UNIXGetTickCountMicro()
    S64 uSecs = t.tv_usec - sg_startTime.tv_usec;
 
    return (U32)(secs * 1000000 + uSecs);
-}
-
-U32 Platform::getRealMilliseconds()
-{
-   return x86UNIXGetTickCount();
-}
-
-U32 Platform::getRealMicroseconds()
-{
-   return x86UNIXGetTickCountMicro();
 }
 
 class UnixTimer
